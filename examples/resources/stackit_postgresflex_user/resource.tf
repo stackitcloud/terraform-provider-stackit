9d7628a98483a46846de15e9a1508d5b350efd3d
Vicente Pinto <vicente.pinto@freiheit.com>
2023-09-07 12:11:14 +0100

    Format example (#1)

--- examples/resources/stackit_postgresflex_user/resource.tf
+++ examples/resources/stackit_postgresflex_user/resource.tf
@@ -2,5 +2,5 @@ resource "stackit_postgresflex_user" "example" {
   project_id  = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
   instance_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
   username    = "username"
-  roles = ["role"]
+  roles       = ["role"]
 }
