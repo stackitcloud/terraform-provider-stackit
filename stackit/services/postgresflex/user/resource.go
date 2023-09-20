e5df522cc7e10a3ba60dac475a93a7b558a29759
vicentepinto98 <vicente.pinto@freiheit.com>
2023-09-20 17:54:02 +0100

    Add documentation

--- stackit/services/postgresflex/user/resource.go
+++ stackit/services/postgresflex/user/resource.go
@@ -321,6 +321,7 @@ func (r *userResource) ImportState(ctx context.Context, req resource.ImportState
 	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("instance_id"), idParts[1])...)
 	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("user_id"), idParts[2])...)
 	tflog.Info(ctx, "Postgresflex user state imported")
+	tflog.Warn(ctx, "The user password is not imported as it is only available upon creation of a new user. The password field will be empty.")
 }
 
 func mapFieldsCreate(userResp *postgresflex.CreateUserResponse, model *Model) error {
