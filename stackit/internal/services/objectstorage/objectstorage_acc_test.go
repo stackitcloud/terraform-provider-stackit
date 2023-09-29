98b83727a5ac1cb4c6145428e9c2f5c629a29b51
Henrique Santos <henrique.santos@freiheit.com>
2023-09-29 10:29:07 +0100

    Implement check destroy

--- stackit/internal/services/objectstorage/objectstorage_acc_test.go
+++ stackit/internal/services/objectstorage/objectstorage_acc_test.go
@@ -1,12 +1,18 @@
 package objectstorage_test
 
 import (
+	"context"
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/stackitcloud/stackit-sdk-go/core/config"
+	"github.com/stackitcloud/stackit-sdk-go/core/utils"
+	"github.com/stackitcloud/stackit-sdk-go/services/objectstorage"
+	"github.com/stackitcloud/terraform-provider-stackit/stackit/internal/core"
 	"github.com/stackitcloud/terraform-provider-stackit/stackit/internal/testutil"
 )
 
@@ -34,6 +40,7 @@ func resourceConfig() string {
 func TestAccObjectStorageResource(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: testutil.TestAccProtoV6ProviderFactories,
+		CheckDestroy:             testAccCheckObjectStorageDestroy,
 		Steps: []resource.TestStep{
 
 			// Creation
@@ -96,4 +103,54 @@ func TestAccObjectStorageResource(t *testing.T) {
 			// Deletion is done by the framework implicitly
 		},
 	})
+}
+
+func testAccCheckObjectStorageDestroy(s *terraform.State) error {
+	ctx := context.Background()
+	var client *objectstorage.APIClient
+	var err error
+	if testutil.ObjectStorageCustomEndpoint == "" {
+		client, err = objectstorage.NewAPIClient()
+	} else {
+		client, err = objectstorage.NewAPIClient(
+			config.WithEndpoint(testutil.ObjectStorageCustomEndpoint),
+		)
+	}
+	if err != nil {
+		return fmt.Errorf("creating client: %w", err)
+	}
+
+	bucketsToDestroy := []string{}
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "stackit_objectstorage_bucket" {
+			continue
+		}
+		// bucket terraform ID: "[project_id],[bucket_name]"
+		bucketName := strings.Split(rs.Primary.ID, core.Separator)[1]
+		bucketsToDestroy = append(bucketsToDestroy, bucketName)
+	}
+
+	bucketsResp, err := client.GetBuckets(ctx, testutil.ProjectId).Execute()
+	if err != nil {
+		return fmt.Errorf("getting bucketsResp: %w", err)
+	}
+
+	buckets := *bucketsResp.Buckets
+	for _, bucket := range buckets {
+		if bucket.Name == nil {
+			continue
+		}
+		bucketName := *bucket.Name
+		if utils.Contains(bucketsToDestroy, *bucket.Name) {
+			_, err := client.DeleteBucketExecute(ctx, testutil.ProjectId, bucketName)
+			if err != nil {
+				return fmt.Errorf("destroying bucket %s during CheckDestroy: %w", bucketName, err)
+			}
+			_, err = objectstorage.DeleteBucketWaitHandler(ctx, client, testutil.ProjectId, bucketName).WaitWithContext(ctx)
+			if err != nil {
+				return fmt.Errorf("destroying instance %s during CheckDestroy: waiting for deletion %w", bucketName, err)
+			}
+		}
+	}
+	return nil
 }
