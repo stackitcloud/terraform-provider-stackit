891124d5bb5d6ce969111d05108334e799cd717f
Henrique Santos <henrique.santos@freiheit.com>
2023-10-11 17:19:26 +0100

    Add TestEnableProject

--- stackit/internal/services/objectstorage/bucket/resource_test.go
+++ stackit/internal/services/objectstorage/bucket/resource_test.go
@@ -1,6 +1,8 @@
 package objectstorage
 
 import (
+	"context"
+	"fmt"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -8,6 +10,19 @@ import (
 	"github.com/stackitcloud/stackit-sdk-go/core/utils"
 	"github.com/stackitcloud/stackit-sdk-go/services/objectstorage"
 )
+
+type objectStorageClientMocked struct {
+	enableFails              bool
+	createProjectExecuteResp *objectstorage.GetProjectResponse
+}
+
+func (c *objectStorageClientMocked) CreateProjectExecute(_ context.Context, _ string) (*objectstorage.GetProjectResponse, error) {
+	if c.enableFails {
+		return nil, fmt.Errorf("create project failed")
+	}
+
+	return c.createProjectExecuteResp, nil
+}
 
 func TestMapFields(t *testing.T) {
 	tests := []struct {
@@ -95,6 +110,75 @@ func TestMapFields(t *testing.T) {
 				if diff != "" {
 					t.Fatalf("Data does not match: %s", diff)
 				}
+			}
+		})
+	}
+}
+
+func TestEnableProject(t *testing.T) {
+	tests := []struct {
+		description string
+		mockedResp  *objectstorage.GetProjectResponse
+		expected    Model
+		enableFails bool
+		isValid     bool
+	}{
+		{
+			"default_values",
+			&objectstorage.GetProjectResponse{},
+			Model{
+				Id:                    types.StringValue("pid,bname"),
+				BucketName:            types.StringValue("bname"),
+				ProjectId:             types.StringValue("pid"),
+				URLPathStyle:          types.StringNull(),
+				URLVirtualHostedStyle: types.StringNull(),
+			},
+			false,
+			true,
+		},
+		{
+			"nil_response",
+			nil,
+			Model{
+				Id:                    types.StringValue("pid,bname"),
+				BucketName:            types.StringValue("bname"),
+				ProjectId:             types.StringValue("pid"),
+				URLPathStyle:          types.StringNull(),
+				URLVirtualHostedStyle: types.StringNull(),
+			},
+			false,
+			true,
+		},
+		{
+			"error_response",
+			&objectstorage.GetProjectResponse{},
+			Model{
+				Id:                    types.StringValue("pid,bname"),
+				BucketName:            types.StringValue("bname"),
+				ProjectId:             types.StringValue("pid"),
+				URLPathStyle:          types.StringNull(),
+				URLVirtualHostedStyle: types.StringNull(),
+			},
+			true,
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.description, func(t *testing.T) {
+			client := &objectStorageClientMocked{
+				enableFails:              tt.enableFails,
+				createProjectExecuteResp: tt.mockedResp,
+			}
+			model := &Model{
+				ProjectId:  tt.expected.ProjectId,
+				BucketName: tt.expected.BucketName,
+			}
+			err := enableProject(context.Background(), model, client)
+			if !tt.isValid && err == nil {
+				t.Fatalf("Should have failed")
+			}
+			if tt.isValid && err != nil {
+				t.Fatalf("Should not have failed: %v", err)
 			}
 		})
 	}
