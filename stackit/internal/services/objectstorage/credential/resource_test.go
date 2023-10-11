7369aee76cc76e54fdbecfedeba34e06281db5bc
Henrique Santos <henrique.santos@freiheit.com>
2023-10-11 18:13:06 +0100

    Add readCredentials test

--- stackit/internal/services/objectstorage/credential/resource_test.go
+++ stackit/internal/services/objectstorage/credential/resource_test.go
@@ -2,13 +2,17 @@ package objectstorage
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/hashicorp/terraform-plugin-framework-timetypes/timetypes"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/stackitcloud/stackit-sdk-go/core/config"
 	"github.com/stackitcloud/stackit-sdk-go/core/utils"
 	"github.com/stackitcloud/stackit-sdk-go/services/objectstorage"
 )
@@ -205,4 +209,185 @@ func TestEnableProject(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestReadCredentials(t *testing.T) {
+	timeValue := time.Now()
+
+	tests := []struct {
+		description         string
+		mockedResp          *objectstorage.GetAccessKeysResponse
+		expected            Model
+		getCredentialsFails bool
+		isValid             bool
+	}{
+		{
+			"default_values",
+			&objectstorage.GetAccessKeysResponse{
+				AccessKeys: &[]objectstorage.AccessKey{
+					{
+						KeyId: utils.Ptr("foo-cid"),
+					},
+					{
+						KeyId: utils.Ptr("bar-cid"),
+					},
+					{
+						KeyId: utils.Ptr("cid"),
+					},
+				},
+			},
+			Model{
+				Id:                  types.StringValue("pid,cgid,cid"),
+				ProjectId:           types.StringValue("pid"),
+				CredentialsGroupId:  types.StringValue("cgid"),
+				CredentialId:        types.StringValue("cid"),
+				Name:                types.StringNull(),
+				AccessKey:           types.StringNull(),
+				SecretAccessKey:     types.StringNull(),
+				ExpirationTimestamp: timetypes.NewRFC3339Null(),
+			},
+			false,
+			true,
+		},
+		{
+			"simple_values",
+			&objectstorage.GetAccessKeysResponse{
+				AccessKeys: &[]objectstorage.AccessKey{
+					{
+						KeyId:       utils.Ptr("foo-cid"),
+						DisplayName: utils.Ptr("foo-name"),
+						Expires:     utils.Ptr(timeValue.Add(time.Hour).Format(time.RFC3339)),
+					},
+					{
+						KeyId:       utils.Ptr("bar-cid"),
+						DisplayName: utils.Ptr("bar-name"),
+						Expires:     utils.Ptr(timeValue.Add(time.Minute).Format(time.RFC3339)),
+					},
+					{
+						KeyId:       utils.Ptr("cid"),
+						DisplayName: utils.Ptr("name"),
+						Expires:     utils.Ptr(timeValue.Format(time.RFC3339)),
+					},
+				},
+			},
+			Model{
+				Id:                  types.StringValue("pid,cgid,cid"),
+				ProjectId:           types.StringValue("pid"),
+				CredentialsGroupId:  types.StringValue("cgid"),
+				CredentialId:        types.StringValue("cid"),
+				Name:                types.StringValue("name"),
+				AccessKey:           types.StringNull(),
+				SecretAccessKey:     types.StringNull(),
+				ExpirationTimestamp: timetypes.NewRFC3339TimeValue(timeValue),
+			},
+			false,
+			true,
+		},
+		{
+			"empty_credentials",
+			&objectstorage.GetAccessKeysResponse{
+				AccessKeys: &[]objectstorage.AccessKey{},
+			},
+			Model{},
+			false,
+			false,
+		},
+		{
+			"nil_response",
+			nil,
+			Model{},
+			false,
+			false,
+		},
+		{
+			"non_matching_credential",
+			&objectstorage.GetAccessKeysResponse{
+				AccessKeys: &[]objectstorage.AccessKey{
+					{
+						KeyId:       utils.Ptr("foo-cid"),
+						DisplayName: utils.Ptr("foo-name"),
+						Expires:     utils.Ptr(timeValue.Add(time.Hour).Format(time.RFC3339)),
+					},
+					{
+						KeyId:       utils.Ptr("bar-cid"),
+						DisplayName: utils.Ptr("bar-name"),
+						Expires:     utils.Ptr(timeValue.Add(time.Minute).Format(time.RFC3339)),
+					},
+				},
+			},
+			Model{},
+			false,
+			false,
+		},
+		{
+			"error_response",
+			&objectstorage.GetAccessKeysResponse{
+				AccessKeys: &[]objectstorage.AccessKey{
+					{
+						KeyId:       utils.Ptr("cid"),
+						DisplayName: utils.Ptr("name"),
+						Expires:     utils.Ptr(timeValue.Format(time.RFC3339)),
+					},
+				},
+			},
+			Model{},
+			true,
+			false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.description, func(t *testing.T) {
+			mockedRespBytes, err := json.Marshal(tt.mockedResp)
+			if err != nil {
+				t.Fatalf("Failed to marshal mocked response: %v", err)
+			}
+
+			handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				if tt.getCredentialsFails {
+					w.WriteHeader(http.StatusBadGateway)
+					w.Header().Set("Content-Type", "application/json")
+					_, err := w.Write([]byte("{\"message\": \"Something bad happened\""))
+					if err != nil {
+						t.Errorf("Failed to write bad response: %v", err)
+					}
+					return
+				}
+
+				_, err := w.Write(mockedRespBytes)
+				if err != nil {
+					t.Errorf("Failed to write response: %v", err)
+				}
+			})
+			mockedServer := httptest.NewServer(handler)
+			defer mockedServer.Close()
+			client, err := objectstorage.NewAPIClient(
+				config.WithEndpoint(mockedServer.URL),
+			)
+			if err != nil {
+				t.Fatalf("Failed to initialize client: %v", err)
+			}
+
+			model := &Model{
+				ProjectId:          tt.expected.ProjectId,
+				CredentialsGroupId: tt.expected.CredentialsGroupId,
+				CredentialId:       tt.expected.CredentialId,
+			}
+			err = readCredentials(context.Background(), model, client)
+			if !tt.isValid && err == nil {
+				t.Fatalf("Should have failed")
+			}
+			if tt.isValid && err != nil {
+				t.Fatalf("Should not have failed: %v", err)
+			}
+			if tt.isValid {
+				diff := cmp.Diff(model, &tt.expected)
+				if diff != "" {
+					t.Fatalf("Data does not match: %s", diff)
+				}
+			}
+		})
+	}
+
 }
