4985bf1caf6cf0f103419950dbafe6e99f567c8c
Henrique Santos <henrique.santos@freiheit.com>
2023-10-13 18:14:05 +0100

    Lint examples

--- examples/data-sources/stackit_object_storage_bucket/data-source.tf
+++ examples/data-sources/stackit_object_storage_bucket/data-source.tf
@@ -1,4 +1,4 @@
 data "stackit_objectstorage_bucket" "example" {
-  project_id  = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-  name = "example-name"
+  project_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  name       = "example-name"
 }
