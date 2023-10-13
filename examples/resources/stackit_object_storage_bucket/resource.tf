978130c4eddb9bb5d3f917a79ca79b948e740aab
Henrique Santos <henrique.santos@freiheit.com>
2023-10-13 18:10:10 +0100

    Fix examples in wrong folder

--- examples/resources/stackit_object_storage_bucket/resource.tf
+++ examples/resources/stackit_object_storage_bucket/resource.tf
@@ -1,4 +1,4 @@
-resource "stackit_object_storage_credentials_group" "example" {
-  project_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-  name       = "example-credentials-group"
+resource "stackit_object_storage_bucket" "example" {
+  project_id  = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  name = "example-bucket"
 }
