60f83a7d2359b1741f6bad1982df668c0ba89be4
Henrique Santos <henrique.santos@freiheit.com>
2023-10-13 18:09:07 +0100

    Add example

---
+++ examples/resources/stackit_object_storage_credential/resource.tf
@@ -0,0 +1,5 @@
+resource "stackit_object_storage_credential" "example" {
+  project_id           = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  credentials_group_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  expiration_timestamp = "2027-01-02T03:04:05Z"
+}
