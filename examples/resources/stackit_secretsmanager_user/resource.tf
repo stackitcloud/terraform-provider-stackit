8784f65b30cde9a7b2f47a51275b6c3603a93f01
vicentepinto98 <vicente.pinto@freiheit.com>
2024-03-06 16:52:42 +0000

    Fix example

--- examples/resources/stackit_secretsmanager_user/resource.tf
+++ examples/resources/stackit_secretsmanager_user/resource.tf
@@ -1,4 +1,4 @@
-resource "stackit_postgresflex_user" "example" {
+resource "stackit_secretsmanager_user" "example" {
   project_id    = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
   instance_id   = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
   description   = "Example user"
