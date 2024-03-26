dfb2c0f57a11948aeddadb4d8cf2ee0e9a19b843
Diogo Ferrão <diogo.ferrao@freiheit.com>
2024-03-26 10:26:35 +0000

    fix sigsegv in test, acl description

--- stackit/internal/services/argus/instance/resource.go
+++ stackit/internal/services/argus/instance/resource.go
@@ -254,7 +254,7 @@ func (r *instanceResource) Schema(_ context.Context, _ resource.SchemaRequest, r
 				Computed: true,
 			},
 			"acl": schema.SetAttribute{
-				Description: "The access control list for this instance. Each entry is a single IP address that is permitted to access, in CIDR notation (/32).",
+				Description: "The access control list for this instance. Each entry is an IP address range that is permitted to access, in CIDR notation.",
 				ElementType: types.StringType,
 				Optional:    true,
 				Validators: []validator.Set{
@@ -604,6 +604,9 @@ func mapFields(ctx context.Context, r *argus.GetInstanceResponse, acl *argus.Lis
 
 func mapACLField(aclList *argus.ListACLResponse, model *Model) error {
 	if aclList == nil || aclList.Acl == nil || len(*aclList.Acl) == 0 {
+		if model.ACL.IsNull() || model.ACL.IsUnknown() {
+			model.ACL = types.SetNull(types.StringType)
+		}
 		return nil
 	}
 
