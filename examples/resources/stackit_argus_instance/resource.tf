33ed6eeb7aa8a4ca7d3b2bd70b67919326d607fd
Diogo Ferrão <diogo.ferrao@freiheit.com>
2024-06-12 11:16:38 +0100

    update argus instance examples

--- examples/resources/stackit_argus_instance/resource.tf
+++ examples/resources/stackit_argus_instance/resource.tf
@@ -3,7 +3,7 @@ resource "stackit_argus_instance" "example" {
   name                                   = "example-instance"
   plan_name                              = "Monitoring-Medium-EU01"
   acl                                    = ["1.1.1.1/32", "2.2.2.2/32"]
-  metrics_retention_days                 = 7
-  metrics_retention_days_5m_downsampling = 30
-  metrics_retention_days_1h_downsampling = 365
+  metrics_retention_days                 = 365
+  metrics_retention_days_5m_downsampling = 60
+  metrics_retention_days_1h_downsampling = 10
 }
