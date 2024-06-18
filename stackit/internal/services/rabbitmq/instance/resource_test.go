f22333aa77e13baa96616e5b38e3d171b5a2ef19
Diogo Ferrão <diogo.ferrao@freiheit.com>
2024-06-18 17:43:24 +0100

    improve testing

--- stackit/internal/services/rabbitmq/instance/resource_test.go
+++ stackit/internal/services/rabbitmq/instance/resource_test.go
@@ -1,14 +1,60 @@
 package rabbitmq
 
 import (
+	"context"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 	"github.com/stackitcloud/stackit-sdk-go/core/utils"
 	"github.com/stackitcloud/stackit-sdk-go/services/rabbitmq"
 )
+
+var fixtureModelParameters = types.ObjectValueMust(parametersTypes, map[string]attr.Value{
+	"sgw_acl":                types.StringValue("acl"),
+	"consumer_timeout":       types.Int64Value(10),
+	"enable_monitoring":      types.BoolValue(true),
+	"graphite":               types.StringValue("1.1.1.1:91"),
+	"max_disk_threshold":     types.Int64Value(100),
+	"metrics_frequency":      types.Int64Value(10),
+	"metrics_prefix":         types.StringValue("prefix"),
+	"monitoring_instance_id": types.StringValue("mid"),
+	"plugins": types.ListValueMust(types.StringType, []attr.Value{
+		types.StringValue("plugin1"),
+		types.StringValue("plugin2"),
+	}),
+	"roles": types.ListValueMust(types.StringType, []attr.Value{
+		types.StringValue("role1"),
+		types.StringValue("role2"),
+	}),
+	"syslog": types.ListValueMust(types.StringType, []attr.Value{
+		types.StringValue("syslog"),
+		types.StringValue("syslog2"),
+	}),
+	"tls_ciphers": types.ListValueMust(types.StringType, []attr.Value{
+		types.StringValue("ciphers1"),
+		types.StringValue("ciphers2"),
+	}),
+	"tls_protocols": types.StringValue("protocol1"),
+})
+
+var fixtureInstanceParameters = rabbitmq.InstanceParameters{
+	SgwAcl:               utils.Ptr("acl"),
+	ConsumerTimeout:      utils.Ptr(int64(10)),
+	EnableMonitoring:     utils.Ptr(true),
+	Graphite:             utils.Ptr("1.1.1.1:91"),
+	MaxDiskThreshold:     utils.Ptr(int64(100)),
+	MetricsFrequency:     utils.Ptr(int64(10)),
+	MetricsPrefix:        utils.Ptr("prefix"),
+	MonitoringInstanceId: utils.Ptr("mid"),
+	Plugins:              &[]string{"plugin1", "plugin2"},
+	Roles:                &[]string{"role1", "role2"},
+	Syslog:               &[]string{"syslog", "syslog2"},
+	TlsCiphers:           &[]string{"ciphers1", "ciphers2"},
+	TlsProtocols:         utils.Ptr("protocol1"),
+}
 
 func TestMapFields(t *testing.T) {
 	tests := []struct {
@@ -47,50 +93,6 @@ func TestMapFields(t *testing.T) {
 				Name:               utils.Ptr("name"),
 				CfOrganizationGuid: utils.Ptr("org"),
 				Parameters: &map[string]interface{}{
-					"sgw_acl": "acl",
-				},
-			},
-			Model{
-				Id:                 types.StringValue("pid,iid"),
-				InstanceId:         types.StringValue("iid"),
-				ProjectId:          types.StringValue("pid"),
-				PlanId:             types.StringValue("plan"),
-				Name:               types.StringValue("name"),
-				CfGuid:             types.StringValue("cf"),
-				CfSpaceGuid:        types.StringValue("space"),
-				DashboardUrl:       types.StringValue("dashboard"),
-				ImageUrl:           types.StringValue("image"),
-				CfOrganizationGuid: types.StringValue("org"),
-				Parameters: types.ObjectValueMust(parametersTypes, map[string]attr.Value{
-					"sgw_acl":                types.StringValue("acl"),
-					"consumer_timeout":       types.Int64Null(),
-					"enable_monitoring":      types.BoolNull(),
-					"graphite":               types.StringNull(),
-					"max_disk_threshold":     types.Int64Null(),
-					"metrics_frequency":      types.Int64Null(),
-					"metrics_prefix":         types.StringNull(),
-					"monitoring_instance_id": types.StringNull(),
-					"plugins":                types.ListNull(types.StringType),
-					"roles":                  types.ListNull(types.StringType),
-					"syslog":                 types.ListNull(types.StringType),
-					"tls_ciphers":            types.ListNull(types.StringType),
-					"tls_protocols":          types.StringNull(),
-				}),
-			},
-			true,
-		},
-		{
-			"simple_values_params",
-			&rabbitmq.Instance{
-				PlanId:             utils.Ptr("plan"),
-				CfGuid:             utils.Ptr("cf"),
-				CfSpaceGuid:        utils.Ptr("space"),
-				DashboardUrl:       utils.Ptr("dashboard"),
-				ImageUrl:           utils.Ptr("image"),
-				InstanceId:         utils.Ptr("iid"),
-				Name:               utils.Ptr("name"),
-				CfOrganizationGuid: utils.Ptr("org"),
-				Parameters: &map[string]interface{}{
 					"sgw_acl":                "acl",
 					"consumer_timeout":       10,
 					"enable_monitoring":      true,
@@ -117,36 +119,11 @@ func TestMapFields(t *testing.T) {
 				DashboardUrl:       types.StringValue("dashboard"),
 				ImageUrl:           types.StringValue("image"),
 				CfOrganizationGuid: types.StringValue("org"),
-				Parameters: types.ObjectValueMust(parametersTypes, map[string]attr.Value{
-					"sgw_acl":                types.StringValue("acl"),
-					"consumer_timeout":       types.Int64Value(10),
-					"enable_monitoring":      types.BoolValue(true),
-					"graphite":               types.StringValue("1.1.1.1:91"),
-					"max_disk_threshold":     types.Int64Value(100),
-					"metrics_frequency":      types.Int64Value(10),
-					"metrics_prefix":         types.StringValue("prefix"),
-					"monitoring_instance_id": types.StringValue("mid"),
-					"plugins": types.ListValueMust(types.StringType, []attr.Value{
-						types.StringValue("plugin1"),
-						types.StringValue("plugin2"),
-					}),
-					"roles": types.ListValueMust(types.StringType, []attr.Value{
-						types.StringValue("role1"),
-						types.StringValue("role2"),
-					}),
-					"syslog": types.ListValueMust(types.StringType, []attr.Value{
-						types.StringValue("syslog"),
-						types.StringValue("syslog2"),
-					}),
-					"tls_ciphers": types.ListValueMust(types.StringType, []attr.Value{
-						types.StringValue("ciphers1"),
-						types.StringValue("ciphers2"),
-					}),
-					"tls_protocols": types.StringValue("protocol1"),
-				}),
+				Parameters:         fixtureModelParameters,
 			},
 			true,
 		},
+
 		{
 			"nil_response",
 			nil,
@@ -205,16 +182,14 @@ func TestMapFields(t *testing.T) {
 
 func TestToCreatePayload(t *testing.T) {
 	tests := []struct {
-		description     string
-		input           *Model
-		inputParameters *parametersModel
-		expected        *rabbitmq.CreateInstancePayload
-		isValid         bool
+		description string
+		input       *Model
+		expected    *rabbitmq.CreateInstancePayload
+		isValid     bool
 	}{
 		{
 			"default_values",
 			&Model{},
-			&parametersModel{},
 			&rabbitmq.CreateInstancePayload{
 				Parameters: &rabbitmq.InstanceParameters{},
 			},
@@ -223,97 +198,34 @@ func TestToCreatePayload(t *testing.T) {
 		{
 			"simple_values",
 			&Model{
-				Name:   types.StringValue("name"),
-				PlanId: types.StringValue("plan"),
-			},
-			&parametersModel{
-				SgwAcl: types.StringValue("sgw"),
+				Name:       types.StringValue("name"),
+				PlanId:     types.StringValue("plan"),
+				Parameters: fixtureModelParameters,
 			},
 			&rabbitmq.CreateInstancePayload{
 				InstanceName: utils.Ptr("name"),
-				Parameters: &rabbitmq.InstanceParameters{
-					SgwAcl: utils.Ptr("sgw"),
-				},
-				PlanId: utils.Ptr("plan"),
-			},
-			true,
-		},
-		{
-			"simple_values_params",
-			&Model{
-				Name:   types.StringValue("name"),
-				PlanId: types.StringValue("plan"),
-			},
-			&parametersModel{
-				SgwAcl:               types.StringValue("sgw"),
-				ConsumerTimeout:      types.Int64Value(10),
-				EnableMonitoring:     types.BoolValue(true),
-				Graphite:             types.StringValue("1.1.1.1:91"),
-				MaxDiskThreshold:     types.Int64Value(100),
-				MetricsFrequency:     types.Int64Value(10),
-				MetricsPrefix:        types.StringValue("prefix"),
-				MonitoringInstanceId: types.StringValue("mid"),
-				Plugins: types.ListValueMust(types.StringType, []attr.Value{
-					types.StringValue("plugin1"),
-					types.StringValue("plugin2"),
-				}),
-				Roles: types.ListValueMust(types.StringType, []attr.Value{
-					types.StringValue("role1"),
-					types.StringValue("role2"),
-				}),
-				Syslog: types.ListValueMust(types.StringType, []attr.Value{
-					types.StringValue("syslog"),
-					types.StringValue("syslog2"),
-				}),
-				TlsCiphers: types.ListValueMust(types.StringType, []attr.Value{
-					types.StringValue("ciphers1"),
-					types.StringValue("ciphers2"),
-				}),
-				TlsProtocols: types.StringValue("protocol1"),
-			},
-			&rabbitmq.CreateInstancePayload{
-				InstanceName: utils.Ptr("name"),
-				Parameters: &rabbitmq.InstanceParameters{
-					SgwAcl:               utils.Ptr("sgw"),
-					ConsumerTimeout:      utils.Ptr(int64(10)),
-					EnableMonitoring:     utils.Ptr(true),
-					Graphite:             utils.Ptr("1.1.1.1:91"),
-					MaxDiskThreshold:     utils.Ptr(int64(100)),
-					MetricsFrequency:     utils.Ptr(int64(10)),
-					MetricsPrefix:        utils.Ptr("prefix"),
-					MonitoringInstanceId: utils.Ptr("mid"),
-					Plugins:              &[]string{"plugin1", "plugin2"},
-					Roles:                &[]string{"role1", "role2"},
-					Syslog:               &[]string{"syslog", "syslog2"},
-					TlsCiphers:           &[]string{"ciphers1", "ciphers2"},
-					TlsProtocols:         utils.Ptr("protocol1"),
-				},
-				PlanId: utils.Ptr("plan"),
+				Parameters:   &fixtureInstanceParameters,
+				PlanId:       utils.Ptr("plan"),
 			},
 			true,
 		},
 		{
 			"null_fields_and_int_conversions",
 			&Model{
-				Name:   types.StringValue(""),
-				PlanId: types.StringValue(""),
-			},
-			&parametersModel{
-				SgwAcl: types.StringNull(),
+				Name:       types.StringValue(""),
+				PlanId:     types.StringValue(""),
+				Parameters: fixtureModelParameters,
 			},
 			&rabbitmq.CreateInstancePayload{
 				InstanceName: utils.Ptr(""),
-				Parameters: &rabbitmq.InstanceParameters{
-					SgwAcl: nil,
-				},
-				PlanId: utils.Ptr(""),
+				PlanId:       utils.Ptr(""),
+				Parameters:   &fixtureInstanceParameters,
 			},
 			true,
 		},
 		{
 			"nil_model",
 			nil,
-			&parametersModel{},
 			nil,
 			false,
 		},
@@ -323,17 +235,26 @@ func TestToCreatePayload(t *testing.T) {
 				Name:   types.StringValue("name"),
 				PlanId: types.StringValue("plan"),
 			},
-			nil,
 			&rabbitmq.CreateInstancePayload{
 				InstanceName: utils.Ptr("name"),
 				PlanId:       utils.Ptr("plan"),
+				Parameters:   &rabbitmq.InstanceParameters{},
 			},
 			true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.description, func(t *testing.T) {
-			output, err := toCreatePayload(tt.input, tt.inputParameters)
+			var parameters = &parametersModel{}
+			if tt.input != nil {
+				if !(tt.input.Parameters.IsNull() || tt.input.Parameters.IsUnknown()) {
+					diags := tt.input.Parameters.As(context.Background(), parameters, basetypes.ObjectAsOptions{})
+					if diags.HasError() {
+						t.Fatalf("Error converting parameters: %v", diags.Errors())
+					}
+				}
+			}
+			output, err := toCreatePayload(tt.input, parameters)
 			if !tt.isValid && err == nil {
 				t.Fatalf("Should have failed")
 			}
@@ -352,16 +273,14 @@ func TestToCreatePayload(t *testing.T) {
 
 func TestToUpdatePayload(t *testing.T) {
 	tests := []struct {
-		description     string
-		input           *Model
-		inputParameters *parametersModel
-		expected        *rabbitmq.PartialUpdateInstancePayload
-		isValid         bool
+		description string
+		input       *Model
+		expected    *rabbitmq.PartialUpdateInstancePayload
+		isValid     bool
 	}{
 		{
 			"default_values",
 			&Model{},
-			&parametersModel{},
 			&rabbitmq.PartialUpdateInstancePayload{
 				Parameters: &rabbitmq.InstanceParameters{},
 			},
@@ -370,92 +289,31 @@ func TestToUpdatePayload(t *testing.T) {
 		{
 			"simple_values",
 			&Model{
-				PlanId: types.StringValue("plan"),
-			},
-			&parametersModel{
-				SgwAcl: types.StringValue("sgw"),
-			},
-			&rabbitmq.PartialUpdateInstancePayload{
-				Parameters: &rabbitmq.InstanceParameters{
-					SgwAcl: utils.Ptr("sgw"),
-				},
-				PlanId: utils.Ptr("plan"),
-			},
-			true,
-		},
-		{
-			"simple_values_params",
-			&Model{
-				Name:   types.StringValue("name"),
-				PlanId: types.StringValue("plan"),
-			},
-			&parametersModel{
-				SgwAcl:               types.StringValue("sgw"),
-				ConsumerTimeout:      types.Int64Value(10),
-				EnableMonitoring:     types.BoolValue(true),
-				Graphite:             types.StringValue("1.1.1.1:91"),
-				MaxDiskThreshold:     types.Int64Value(100),
-				MetricsFrequency:     types.Int64Value(10),
-				MetricsPrefix:        types.StringValue("prefix"),
-				MonitoringInstanceId: types.StringValue("mid"),
-				Plugins: types.ListValueMust(types.StringType, []attr.Value{
-					types.StringValue("plugin1"),
-					types.StringValue("plugin2"),
-				}),
-				Roles: types.ListValueMust(types.StringType, []attr.Value{
-					types.StringValue("role1"),
-					types.StringValue("role2"),
-				}),
-				Syslog: types.ListValueMust(types.StringType, []attr.Value{
-					types.StringValue("syslog"),
-					types.StringValue("syslog2"),
-				}),
-				TlsCiphers: types.ListValueMust(types.StringType, []attr.Value{
-					types.StringValue("ciphers1"),
-					types.StringValue("ciphers2"),
-				}),
-				TlsProtocols: types.StringValue("protocol1"),
+				Name:       types.StringValue("name"),
+				PlanId:     types.StringValue("plan"),
+				Parameters: fixtureModelParameters,
 			},
 			&rabbitmq.PartialUpdateInstancePayload{
-				Parameters: &rabbitmq.InstanceParameters{
-					SgwAcl:               utils.Ptr("sgw"),
-					ConsumerTimeout:      utils.Ptr(int64(10)),
-					EnableMonitoring:     utils.Ptr(true),
-					Graphite:             utils.Ptr("1.1.1.1:91"),
-					MaxDiskThreshold:     utils.Ptr(int64(100)),
-					MetricsFrequency:     utils.Ptr(int64(10)),
-					MetricsPrefix:        utils.Ptr("prefix"),
-					MonitoringInstanceId: utils.Ptr("mid"),
-					Plugins:              &[]string{"plugin1", "plugin2"},
-					Roles:                &[]string{"role1", "role2"},
-					Syslog:               &[]string{"syslog", "syslog2"},
-					TlsCiphers:           &[]string{"ciphers1", "ciphers2"},
-					TlsProtocols:         utils.Ptr("protocol1"),
-				},
-				PlanId: utils.Ptr("plan"),
+				Parameters: &fixtureInstanceParameters,
+				PlanId:     utils.Ptr("plan"),
 			},
 			true,
 		},
 		{
 			"null_fields_and_int_conversions",
 			&Model{
-				PlanId: types.StringValue(""),
-			},
-			&parametersModel{
-				SgwAcl: types.StringNull(),
+				PlanId:     types.StringValue(""),
+				Parameters: fixtureModelParameters,
 			},
 			&rabbitmq.PartialUpdateInstancePayload{
-				Parameters: &rabbitmq.InstanceParameters{
-					SgwAcl: nil,
-				},
-				PlanId: utils.Ptr(""),
+				Parameters: &fixtureInstanceParameters,
+				PlanId:     utils.Ptr(""),
 			},
 			true,
 		},
 		{
 			"nil_model",
 			nil,
-			&parametersModel{},
 			nil,
 			false,
 		},
@@ -464,16 +322,25 @@ func TestToUpdatePayload(t *testing.T) {
 			&Model{
 				PlanId: types.StringValue("plan"),
 			},
-			nil,
 			&rabbitmq.PartialUpdateInstancePayload{
-				PlanId: utils.Ptr("plan"),
+				PlanId:     utils.Ptr("plan"),
+				Parameters: &rabbitmq.InstanceParameters{},
 			},
 			true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.description, func(t *testing.T) {
-			output, err := toUpdatePayload(tt.input, tt.inputParameters)
+			var parameters = &parametersModel{}
+			if tt.input != nil {
+				if !(tt.input.Parameters.IsNull() || tt.input.Parameters.IsUnknown()) {
+					diags := tt.input.Parameters.As(context.Background(), parameters, basetypes.ObjectAsOptions{})
+					if diags.HasError() {
+						t.Fatalf("Error converting parameters: %v", diags.Errors())
+					}
+				}
+			}
+			output, err := toUpdatePayload(tt.input, parameters)
 			if !tt.isValid && err == nil {
 				t.Fatalf("Should have failed")
 			}
