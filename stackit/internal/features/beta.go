ca406ef7e04353ba76e14d07ec5f0526d49e8dfb
Diogo Ferrão <diogo.ferrao@freiheit.com>
2024-06-20 17:46:25 +0100

    Fix warning message

--- stackit/internal/features/beta.go
+++ stackit/internal/features/beta.go
@@ -24,7 +24,8 @@ func BetaResourcesEnabled(ctx context.Context, data *core.ProviderData, diags *d
 		if strings.EqualFold(value, "false") {
 			return false
 		}
-		warnDetails := fmt.Sprintf(`The value of the environment variable that enables BETA functionality must be either "true" or "false", got %q. \nDefaulting to the provider feature flag.`, value)
+		warnDetails := fmt.Sprintf(`The value of the environment variable that enables BETA functionality must be either "true" or "false", got %q.
+Defaulting to the provider feature flag.`, value)
 		core.LogAndAddWarning(ctx, diags, "Invalid value for STACKIT_TF_ENABLE_BETA_RESOURCES environment variable.", warnDetails)
 	}
 	// ProviderData should always be set, but we check just in case
