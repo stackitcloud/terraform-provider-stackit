e5efa551b2bafcc300250ad29f85e55684c10f5f
João Palet <joao.palet@freiheit.com>
2024-07-01 12:31:33 +0100

    Fix bug in parameter modelling

--- stackit/internal/services/opensearch/instance/resource.go
+++ stackit/internal/services/opensearch/instance/resource.go
@@ -350,7 +350,7 @@ func (r *instanceResource) Create(ctx context.Context, req resource.CreateReques
 
 	var parameters *parametersModel
 	if !(model.Parameters.IsNull() || model.Parameters.IsUnknown()) {
-		parameters := &parametersModel{}
+		parameters = &parametersModel{}
 		diags = model.Parameters.As(ctx, parameters, basetypes.ObjectAsOptions{})
 		resp.Diagnostics.Append(diags...)
 		if resp.Diagnostics.HasError() {
