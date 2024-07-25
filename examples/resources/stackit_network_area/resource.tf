ed460f6043703232287766d4fec718d870dafcb6
Gökçe Gök Klingel <goekce.goek_klingel@mail.schwarz>
2024-07-25 17:14:01 +0200

    fix linter issues

--- examples/resources/stackit_network_area/resource.tf
+++ examples/resources/stackit_network_area/resource.tf
@@ -1,12 +1,9 @@
 resource "stackit_network_area" "example" {
-  organization_id  = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-  name             = "example-network-area"
-  network_ranges   = [
+  organization_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  name            = "example-network-area"
+  network_ranges = [
     {
       prefix = "1.2.3.4"
-    },
-    {
-      prefix = "5.6.7.8"
     }
   ]
   transfer_network = "1.2.3.4/5"
