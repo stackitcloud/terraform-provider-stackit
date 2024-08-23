6a8b5536b75cedb81f23df7f188ef37bbadf6c27
Gökçe Gök Klingel <goekce.goek_klingel@mail.schwarz>
2024-08-23 09:37:41 +0200

    extend warning about empty uri

--- stackit/internal/services/mongodbflex/user/resource.go
+++ stackit/internal/services/mongodbflex/user/resource.go
@@ -334,8 +334,8 @@ func (r *userResource) ImportState(ctx context.Context, req resource.ImportState
 	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("instance_id"), idParts[1])...)
 	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("user_id"), idParts[2])...)
 	core.LogAndAddWarning(ctx, &resp.Diagnostics,
-		"MongoDB Flex user imported with empty password",
-		"The user password is not imported as it is only available upon creation of a new user. The password field will be empty.",
+		"MongoDB Flex user imported with empty password and empty uri",
+		"The user password and uri are not imported as they are only available upon creation of a new user. The password and uri fields will be empty.",
 	)
 	tflog.Info(ctx, "MongoDB Flex user state imported")
 }
