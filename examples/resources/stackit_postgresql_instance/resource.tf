1f7c7ea26ab5ca6b34ef54cbc4210522208f1df0
Vicente Pinto <vicente.pinto@freiheit.com>
2024-09-04 15:44:08 +0100

    Remove examples

--- examples/resources/stackit_postgresql_instance/resource.tf
+++
@@ -1,9 +0,0 @@
-resource "stackit_postgresql_instance" "example" {
-  project_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-  name       = "example-instance"
-  version    = "10"
-  plan_name  = "example-plan-name"
-  parameters = {
-    sgw_acl = "x.x.x.x/x,y.y.y.y/y"
-  }
-}
