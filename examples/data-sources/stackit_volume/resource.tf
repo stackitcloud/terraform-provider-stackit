3109b4e88519ad601926a5dc6abcbd6093f5899d
Vicente Pinto <vicente.pinto@freiheit.com>
2024-10-02 09:54:36 +0100

    Fix lint

--- examples/data-sources/stackit_volume/resource.tf
+++ examples/data-sources/stackit_volume/resource.tf
@@ -1,4 +1,4 @@
 resource "stackit_volume" "example" {
   project_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-  volume_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  volume_id  = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
 }
--- examples/resources/stackit_volume/resource.tf
+++ examples/resources/stackit_volume/resource.tf
@@ -1,8 +1,8 @@
 resource "stackit_volume" "example" {
-  project_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-  name = "my_volume"
+  project_id        = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  name              = "my_volume"
   availability_zone = "eu01-m"
-  size = 64
+  size              = 64
   labels = {
     "key" = "value"
   }
