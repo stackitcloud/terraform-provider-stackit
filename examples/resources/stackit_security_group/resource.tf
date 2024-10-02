64184624d01400ef2a89b7dee8c3d545a7fc28d6
Gökçe Gök Klingel <goekce.goek_klingel@stackit.cloud>
2024-10-02 17:01:02 +0200

    fix linter issues

--- examples/resources/stackit_security_group/resource.tf
+++ examples/resources/stackit_security_group/resource.tf
@@ -1,6 +1,6 @@
 resource "stackit_security_group" "example" {
-  project_id        = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-  name              = "my_volume"
+  project_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  name       = "my_security_group"
   labels = {
     "key" = "value"
   }
