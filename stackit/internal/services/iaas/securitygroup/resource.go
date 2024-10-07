e35914f9c2f63f46bd91e84fccb11f13d51d119d
Gökçe Gök Klingel <goekce.goek_klingel@stackit.cloud>
2024-10-07 13:27:08 +0200

    sort imports

--- stackit/internal/services/iaas/securitygroup/resource.go
+++ stackit/internal/services/iaas/securitygroup/resource.go
@@ -3,7 +3,6 @@ package securitygroup
 import (
 	"context"
 	"fmt"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/boolplanmodifier"
 	"net/http"
 	"regexp"
 	"strings"
@@ -13,6 +12,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/boolplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
