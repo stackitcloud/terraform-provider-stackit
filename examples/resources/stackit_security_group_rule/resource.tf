fdad192018c3166ca72a7e5e869854628c3e7886
Gökçe Gök Klingel <goekce.goek_klingel@stackit.cloud>
2024-10-10 17:21:24 +0200

    extend example with objects

--- examples/resources/stackit_security_group_rule/resource.tf
+++ examples/resources/stackit_security_group_rule/resource.tf
@@ -2,4 +2,16 @@ resource "stackit_security_group_rule" "example" {
   project_id        = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
   security_group_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
   direction         = "ingress"
+  icmp_parameters = {
+    code = 0
+    type = 8
+  }
+  protocol = {
+    name     = "name"
+    protocol = 1
+  }
+  port_range = {
+    max = 22
+    min = 22
+  }
 }
