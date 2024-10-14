32d74d58888d13d8e9633808e2c8858c87897354
Vicente Pinto <vicente.pinto@freiheit.com>
2024-10-14 16:39:23 +0100

    Fix examples formatting

--- examples/resources/stackit_server_network_interface_attach/resource.tf
+++ examples/resources/stackit_server_network_interface_attach/resource.tf
@@ -1,5 +1,5 @@
 resource "stackit_server_network_interface_attach" "attached_network_interface" {
-  project_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-  server_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  project_id           = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  server_id            = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
   network_interface_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
 }
