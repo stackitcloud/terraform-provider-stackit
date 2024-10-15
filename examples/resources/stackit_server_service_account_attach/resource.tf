21c2c2d5b9a6e3d239f0129202c7fac64b4d02c7
Vicente Pinto <vicente.pinto@freiheit.com>
2024-10-15 09:39:15 +0100

    Server attachment resources (#557)
    * Server attachemnt resources
    
    * Add examples
    
    * Update volume datasource example
    
    * Fix linting issues
    
    * Fix linting
    
    * Fix examples formatting
    
    * Update go.mod

---
+++ examples/resources/stackit_server_service_account_attach/resource.tf
@@ -0,0 +1,5 @@
+resource "stackit_server_service_account_attach" "attached_service_account" {
+  project_id            = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  server_id             = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  service_account_email = "service-account@stackit.cloud"
+}
