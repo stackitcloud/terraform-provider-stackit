79f5155fd5ecbc56bebfe16f8c1346cac1750587
Gökçe Gök Klingel <goekce.goek_klingel@stackit.cloud>
2024-11-11 15:49:16 +0100

    update examples

--- examples/resources/stackit_network/resource.tf
+++ examples/resources/stackit_network/resource.tf
@@ -1,9 +1,23 @@
+resource "stackit_network" "example_with_name" {
+  project_id         = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  name               = "example-network"
+}
+
+resource "stackit_network" "example_routed_network" {
+  project_id         = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  name               = "example-network"
+  labels = {
+    "key" = "value"
+  }
+  routed = true
+}
+
 resource "stackit_network" "example" {
   project_id         = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
   name               = "example-network"
   ipv4_nameservers   = ["1.2.3.4", "5.6.7.8"]
   ipv4_prefix_length = 24
-  ipv4_gateway       = "10.1.2.1"
+  ipv4_gateway       = "10.1.2.3"
   ipv4_prefix        = "10.1.2.0/24"
   labels = {
     "key" = "value"
