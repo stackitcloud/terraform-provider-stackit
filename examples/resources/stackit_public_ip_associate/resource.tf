481d85afda931030b7c0f410ae96ca34d2b55470
Gökçe Gök Klingel <goekce.goek_klingel@stackit.cloud>
2024-11-12 09:56:51 +0100

    add example

---
+++ examples/resources/stackit_public_ip_associate/resource.tf
@@ -0,0 +1,5 @@
+resource "stackit_public_ip_associate" "example" {
+  project_id           = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  public_ip_id         = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  network_interface_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+}
