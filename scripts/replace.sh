90da91c6dc134539ff726fcbde3499d1365a5573
Rüdiger Schmitz <152157960+bahkauv70@users.noreply.github.com>
2025-01-29 10:51:12 +0100

    chore: add script to replace dependencies in a dummy go.work file

---
+++ scripts/replace.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Add replace directives to local files to go.work
+set -eo pipefail
+
+if [ -d $1 ]; then 
+    SDK_DIR=$1
+    shift
+fi
+
+if [ -z "$SDK_DIR" ]; then
+    SDK_DIR=../stackit-sdk-generator/sdk-repo-updated
+    echo "No SDK_DIR set, using $SDK_DIR"
+fi
+
+
+if [ ! -f go.work ]; then
+    go work init
+    go work use .
+else
+    echo "go.work already exists"
+fi
+
+if [ $# -gt 0 ];then
+    # modules passed via commandline
+    for service in $*; do
+        if [ ! -d $SDK_DIR/services/$service ]; then
+            echo "service directory $SDK_DIR/services/$service does not exist"
+            exit 1
+        fi
+        echo "replacing selected service $service"
+        if [ "$service" = "core" ]; then
+            go work edit -replace github.com/stackitcloud/stackit-sdk-go/core=$SDK_DIR/core
+        else
+            go work edit -replace github.com/stackitcloud/stackit-sdk-go/services/$service=$SDK_DIR/services/$service
+        fi
+    done
+else
+    # replace all modules
+    echo "replacing all services"
+    go work edit -replace github.com/stackitcloud/stackit-sdk-go/core=$SDK_DIR/core
+    for n in $(find ${SDK_DIR}/services -name go.mod);do
+        service=$(dirname $n)
+        service=${service#${SDK_DIR}/services/}
+        go work edit -replace github.com/stackitcloud/stackit-sdk-go/services/$service=$(dirname $n)
+    done
+fi
+go work edit -fmt
+go work sync
