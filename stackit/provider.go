3b7f0ec9449e29e545b4fa5fcc5a6180ca79599f
Mauritz Uphoff <mauritz.uphoff@me.com>
2025-03-08 13:27:09 +0100

    feat: implement sa access key resource

--- stackit/provider.go
+++ stackit/provider.go
@@ -66,6 +66,7 @@ import (
 	serverBackupSchedule "github.com/stackitcloud/terraform-provider-stackit/stackit/internal/services/serverbackup/schedule"
 	serverUpdateSchedule "github.com/stackitcloud/terraform-provider-stackit/stackit/internal/services/serverupdate/schedule"
 	serviceAccount "github.com/stackitcloud/terraform-provider-stackit/stackit/internal/services/serviceaccount/account"
+	serviceAccountKey "github.com/stackitcloud/terraform-provider-stackit/stackit/internal/services/serviceaccount/key"
 	serviceAccountToken "github.com/stackitcloud/terraform-provider-stackit/stackit/internal/services/serviceaccount/token"
 	skeCluster "github.com/stackitcloud/terraform-provider-stackit/stackit/internal/services/ske/cluster"
 	skeKubeconfig "github.com/stackitcloud/terraform-provider-stackit/stackit/internal/services/ske/kubeconfig"
@@ -566,6 +567,7 @@ func (p *Provider) Resources(_ context.Context) []func() resource.Resource {
 		serverUpdateSchedule.NewScheduleResource,
 		serviceAccount.NewServiceAccountResource,
 		serviceAccountToken.NewServiceAccountTokenResource,
+		serviceAccountKey.NewServiceAccountKeyResource,
 		skeProject.NewProjectResource,
 		skeCluster.NewClusterResource,
 		skeKubeconfig.NewKubeconfigResource,
