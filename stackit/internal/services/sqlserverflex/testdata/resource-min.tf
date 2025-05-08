5e04deaac3229bd794ab3b32e754825382d613a0
Rüdiger Schmitz <152157960+bahkauv70@users.noreply.github.com>
2025-05-08 16:36:54 +0200

    chore(sqlserverflex): implemented min/max test

---
+++ stackit/internal/services/sqlserverflex/testdata/resource-min.tf
@@ -0,0 +1,33 @@
+variable "project_id" {}
+variable "name" {}
+variable "flavor_cpu" {}
+variable "flavor_ram" {}
+variable "username" {}
+variable "role" {}
+
+resource "stackit_sqlserverflex_instance" "instance" {
+  project_id = var.project_id
+  name       = var.name
+  flavor = {
+    cpu = var.flavor_cpu
+    ram = var.flavor_ram
+  }
+}
+
+resource "stackit_sqlserverflex_user" "user" {
+  project_id  = stackit_sqlserverflex_instance.instance.project_id
+  instance_id = stackit_sqlserverflex_instance.instance.instance_id
+  username    = var.username
+  roles       = [var.role]
+}
+
+data "stackit_sqlserverflex_instance" "instance" {
+  project_id  = var.project_id
+  instance_id = stackit_sqlserverflex_instance.instance.instance_id
+}
+
+data "stackit_sqlserverflex_user" "user" {
+  project_id  = var.project_id
+  instance_id = stackit_sqlserverflex_instance.instance.instance_id
+  user_id     = stackit_sqlserverflex_user.user.user_id
+}
