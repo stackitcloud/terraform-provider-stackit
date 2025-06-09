df74dfa2fc47e24016733211a6db4a6b1ee17191
Mauritz Uphoff <mauritz.uphoff@me.com>
2025-06-09 22:46:38 +0200

    feat: implement advanced search options for images

--- examples/data-sources/stackit_image/data-source.tf
+++ examples/data-sources/stackit_image/data-source.tf
@@ -1,4 +1,29 @@
-data "stackit_image" "example" {
+data "stackit_image" "default" {
   project_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
   image_id   = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+}
+
+data "stackit_image" "name_match" {
+  project_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  name       = "Ubuntu 22.04"
+}
+
+data "stackit_image" "name_regex_latest" {
+  project_id      = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  name_regex      = "^Ubuntu .*"
+  sort_descending = true
+}
+
+data "stackit_image" "name_regex_oldest" {
+  project_id      = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  name_regex      = "^Ubuntu .*"
+  sort_descending = false
+}
+
+data "stackit_image" "filter_distro_version" {
+  project_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  filter = {
+    distro  = "debian"
+    version = "11"
+  }
 }
