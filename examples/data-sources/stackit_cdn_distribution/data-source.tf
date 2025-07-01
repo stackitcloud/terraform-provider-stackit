fc231f735ea635e7cf004923d47e6bf5ce6a3e0e
Matheus Politano <matheus.correia_politano@mail.schwarz>
2025-07-01 10:02:27 +0200

    add example

--- examples/data-sources/stackit_cdn_distribution/data-source.tf
+++ examples/data-sources/stackit_cdn_distribution/data-source.tf
@@ -1,5 +1,8 @@
 data "stackit_cdn_distribution" "example" {
   project_id      = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
   distribution_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  optimizer = {
+							enabled = true
+	}
 }
 
