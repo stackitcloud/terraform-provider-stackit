76b737bcba874d62c9fb2c9ac21a8d8dba54de1a
Matheus Politano <matheus.correia_politano@mail.schwarz>
2025-07-01 10:07:50 +0200

    format the code

--- examples/data-sources/stackit_cdn_distribution/data-source.tf
+++ examples/data-sources/stackit_cdn_distribution/data-source.tf
@@ -2,7 +2,7 @@ data "stackit_cdn_distribution" "example" {
   project_id      = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
   distribution_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
   optimizer = {
-							enabled = true
-	}
+    enabled = true
+  }
 }
 
