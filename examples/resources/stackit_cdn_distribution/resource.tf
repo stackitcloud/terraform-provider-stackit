d1af69c14f38a929183a7d8dadf56ae672c350ae
Christian Hamm <Christian.Hamm@mail.schwarz>
2025-07-15 12:03:47 +0200

    Appeasing the linter

--- examples/resources/stackit_cdn_distribution/resource.tf
+++ examples/resources/stackit_cdn_distribution/resource.tf
@@ -7,7 +7,7 @@ resource "stackit_cdn_distribution" "example_distribution" {
     }
     regions           = ["EU", "US", "ASIA", "AF", "SA"]
     blocked_countries = ["DE", "AT", "CH"]
-    
+
     optimizer = {
       enabled = true
     }
