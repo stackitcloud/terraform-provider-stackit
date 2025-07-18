6acb3c1c3f4cfbb7e875375bc36e184ae1c4541e
Ruben Hoenle <Ruben.Hoenle@stackit.cloud>
2025-07-18 11:39:18 +0200

    feat(observability): add send_resolved field
    relates to STACKITTPR-283

--- stackit/internal/services/observability/instance/resource.go
+++ stackit/internal/services/observability/instance/resource.go
@@ -172,6 +172,7 @@ type emailConfigsModel struct {
 	AuthPassword types.String `tfsdk:"auth_password"`
 	AuthUsername types.String `tfsdk:"auth_username"`
 	From         types.String `tfsdk:"from"`
+	SendResolved types.Bool   `tfsdk:"send_resolved"`
 	Smarthost    types.String `tfsdk:"smart_host"`
 	To           types.String `tfsdk:"to"`
 }
@@ -181,32 +182,37 @@ var emailConfigsTypes = map[string]attr.Type{
 	"auth_password": types.StringType,
 	"auth_username": types.StringType,
 	"from":          types.StringType,
+	"send_resolved": types.BoolType,
 	"smart_host":    types.StringType,
 	"to":            types.StringType,
 }
 
 // Struct corresponding to Model.AlertConfig.receivers.opsGenieConfigs
 type opsgenieConfigsModel struct {
-	ApiKey types.String `tfsdk:"api_key"`
-	ApiUrl types.String `tfsdk:"api_url"`
-	Tags   types.String `tfsdk:"tags"`
+	ApiKey       types.String `tfsdk:"api_key"`
+	ApiUrl       types.String `tfsdk:"api_url"`
+	Tags         types.String `tfsdk:"tags"`
+	SendResolved types.Bool   `tfsdk:"send_resolved"`
 }
 
 var opsgenieConfigsTypes = map[string]attr.Type{
-	"api_key": types.StringType,
-	"api_url": types.StringType,
-	"tags":    types.StringType,
+	"api_key":       types.StringType,
+	"api_url":       types.StringType,
+	"tags":          types.StringType,
+	"send_resolved": types.BoolType,
 }
 
 // Struct corresponding to Model.AlertConfig.receivers.webHooksConfigs
 type webHooksConfigsModel struct {
-	Url     types.String `tfsdk:"url"`
-	MsTeams types.Bool   `tfsdk:"ms_teams"`
+	Url          types.String `tfsdk:"url"`
+	MsTeams      types.Bool   `tfsdk:"ms_teams"`
+	SendResolved types.Bool   `tfsdk:"send_resolved"`
 }
 
 var webHooksConfigsTypes = map[string]attr.Type{
-	"url":      types.StringType,
-	"ms_teams": types.BoolType,
+	"url":           types.StringType,
+	"ms_teams":      types.BoolType,
+	"send_resolved": types.BoolType,
 }
 
 var routeDescriptions = map[string]string{
@@ -598,6 +604,10 @@ func (r *instanceResource) Schema(_ context.Context, _ resource.SchemaRequest, r
 												Description: "The sender email address. Must be a valid email address",
 												Optional:    true,
 											},
+											"send_resolved": schema.BoolAttribute{
+												Description: "Whether to notify about resolved alerts.",
+												Optional:    true,
+											},
 											"smart_host": schema.StringAttribute{
 												Description: "The SMTP host through which emails are sent.",
 												Optional:    true,
@@ -629,6 +639,10 @@ func (r *instanceResource) Schema(_ context.Context, _ resource.SchemaRequest, r
 												Description: "Comma separated list of tags attached to the notifications.",
 												Optional:    true,
 											},
+											"send_resolved": schema.BoolAttribute{
+												Description: "Whether to notify about resolved alerts.",
+												Optional:    true,
+											},
 										},
 									},
 								},
@@ -646,6 +660,10 @@ func (r *instanceResource) Schema(_ context.Context, _ resource.SchemaRequest, r
 											},
 											"ms_teams": schema.BoolAttribute{
 												Description: "Microsoft Teams webhooks require special handling, set this to true if the webhook is for Microsoft Teams.",
+												Optional:    true,
+											},
+											"send_resolved": schema.BoolAttribute{
+												Description: "Whether to notify about resolved alerts.",
 												Optional:    true,
 											},
 										},
@@ -1503,6 +1521,7 @@ func getMockAlertConfig(ctx context.Context) (alertConfigModel, error) {
 	mockEmailConfig, diags := types.ObjectValue(emailConfigsTypes, map[string]attr.Value{
 		"to":            types.StringValue("123@gmail.com"),
 		"smart_host":    types.StringValue("smtp.gmail.com:587"),
+		"send_resolved": types.BoolValue(false),
 		"from":          types.StringValue("xxxx@gmail.com"),
 		"auth_username": types.StringValue("xxxx@gmail.com"),
 		"auth_password": types.StringValue("xxxxxxxxx"),
@@ -1650,6 +1669,7 @@ func mapReceiversToAttributes(ctx context.Context, respReceivers *[]observabilit
 					"auth_password": types.StringPointerValue(emailConfig.AuthPassword),
 					"auth_username": types.StringPointerValue(emailConfig.AuthUsername),
 					"from":          types.StringPointerValue(emailConfig.From),
+					"send_resolved": types.BoolPointerValue(emailConfig.SendResolved),
 					"smart_host":    types.StringPointerValue(emailConfig.Smarthost),
 					"to":            types.StringPointerValue(emailConfig.To),
 				}
@@ -1665,9 +1685,10 @@ func mapReceiversToAttributes(ctx context.Context, respReceivers *[]observabilit
 		if receiver.OpsgenieConfigs != nil {
 			for _, opsgenieConfig := range *receiver.OpsgenieConfigs {
 				opsGenieConfigMap := map[string]attr.Value{
-					"api_key": types.StringPointerValue(opsgenieConfig.ApiKey),
-					"api_url": types.StringPointerValue(opsgenieConfig.ApiUrl),
-					"tags":    types.StringPointerValue(opsgenieConfig.Tags),
+					"api_key":       types.StringPointerValue(opsgenieConfig.ApiKey),
+					"api_url":       types.StringPointerValue(opsgenieConfig.ApiUrl),
+					"tags":          types.StringPointerValue(opsgenieConfig.Tags),
+					"send_resolved": types.BoolPointerValue(opsgenieConfig.SendResolved),
 				}
 				opsGenieConfigModel, diags := types.ObjectValue(opsgenieConfigsTypes, opsGenieConfigMap)
 				if diags.HasError() {
@@ -1681,8 +1702,9 @@ func mapReceiversToAttributes(ctx context.Context, respReceivers *[]observabilit
 		if receiver.WebHookConfigs != nil {
 			for _, webhookConfig := range *receiver.WebHookConfigs {
 				webHookConfigsMap := map[string]attr.Value{
-					"url":      types.StringPointerValue(webhookConfig.Url),
-					"ms_teams": types.BoolPointerValue(webhookConfig.MsTeams),
+					"url":           types.StringPointerValue(webhookConfig.Url),
+					"ms_teams":      types.BoolPointerValue(webhookConfig.MsTeams),
+					"send_resolved": types.BoolPointerValue(webhookConfig.SendResolved),
 				}
 				webHookConfigsModel, diags := types.ObjectValue(webHooksConfigsTypes, webHookConfigsMap)
 				if diags.HasError() {
@@ -1997,6 +2019,7 @@ func toReceiverPayload(ctx context.Context, model *alertConfigModel) (*[]observa
 					AuthPassword: conversion.StringValueToPointer(emailConfig.AuthPassword),
 					AuthUsername: conversion.StringValueToPointer(emailConfig.AuthUsername),
 					From:         conversion.StringValueToPointer(emailConfig.From),
+					SendResolved: conversion.BoolValueToPointer(emailConfig.SendResolved),
 					Smarthost:    conversion.StringValueToPointer(emailConfig.Smarthost),
 					To:           conversion.StringValueToPointer(emailConfig.To),
 				})
@@ -2014,9 +2037,10 @@ func toReceiverPayload(ctx context.Context, model *alertConfigModel) (*[]observa
 			for i := range opsgenieConfigs {
 				opsgenieConfig := opsgenieConfigs[i]
 				payloadOpsGenieConfigs = append(payloadOpsGenieConfigs, observability.CreateAlertConfigReceiverPayloadOpsgenieConfigsInner{
-					ApiKey: conversion.StringValueToPointer(opsgenieConfig.ApiKey),
-					ApiUrl: conversion.StringValueToPointer(opsgenieConfig.ApiUrl),
-					Tags:   conversion.StringValueToPointer(opsgenieConfig.Tags),
+					ApiKey:       conversion.StringValueToPointer(opsgenieConfig.ApiKey),
+					ApiUrl:       conversion.StringValueToPointer(opsgenieConfig.ApiUrl),
+					Tags:         conversion.StringValueToPointer(opsgenieConfig.Tags),
+					SendResolved: conversion.BoolValueToPointer(opsgenieConfig.SendResolved),
 				})
 			}
 			receiverPayload.OpsgenieConfigs = &payloadOpsGenieConfigs
@@ -2032,8 +2056,9 @@ func toReceiverPayload(ctx context.Context, model *alertConfigModel) (*[]observa
 			for i := range receiverWebHooksConfigs {
 				webHooksConfig := receiverWebHooksConfigs[i]
 				payloadWebHooksConfigs = append(payloadWebHooksConfigs, observability.CreateAlertConfigReceiverPayloadWebHookConfigsInner{
-					Url:     conversion.StringValueToPointer(webHooksConfig.Url),
-					MsTeams: conversion.BoolValueToPointer(webHooksConfig.MsTeams),
+					Url:          conversion.StringValueToPointer(webHooksConfig.Url),
+					MsTeams:      conversion.BoolValueToPointer(webHooksConfig.MsTeams),
+					SendResolved: conversion.BoolValueToPointer(webHooksConfig.SendResolved),
 				})
 			}
 			receiverPayload.WebHookConfigs = &payloadWebHooksConfigs
