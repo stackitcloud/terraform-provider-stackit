093fb0de2a79c589dced9ef04b8c040e95163fbc
Alexander Dahmen <alexander.dahmen@inovex.de>
2025-07-28 12:11:20 +0200

    Update network_area and ske documentation and examples (#929)
    Signed-off-by: Alexander Dahmen <alexander.dahmen@inovex.de>

--- examples/resources/stackit_network_area/resource.tf
+++ examples/resources/stackit_network_area/resource.tf
@@ -6,7 +6,7 @@ resource "stackit_network_area" "example" {
       prefix = "192.168.0.0/24"
     }
   ]
-  transfer_network = "192.168.0.0/24"
+  transfer_network = "192.168.1.0/24"
   labels = {
     "key" = "value"
   }
