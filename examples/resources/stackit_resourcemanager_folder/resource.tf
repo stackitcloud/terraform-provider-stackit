cf7315bfc22890bd0f55fbc77823899f8d36726e
Mauritz Uphoff <mauritz.uphoff@stackit.cloud>
2025-09-02 12:33:29 +0200

    feat(resourcemanager): extend folder example
    Signed-off-by: Mauritz Uphoff <mauritz.uphoff@stackit.cloud>

--- examples/resources/stackit_resourcemanager_folder/resource.tf
+++ examples/resources/stackit_resourcemanager_folder/resource.tf
@@ -1,5 +1,16 @@
 resource "stackit_resourcemanager_folder" "example" {
-  name                = "foo"
+  name                = "example-folder"
   owner_email         = "foo.bar@stackit.cloud"
   parent_container_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+}
+
+# Note:
+# You can add projects under folders.
+# However, when deleting a project, be aware:
+#   - Projects may remain "invisible" for up to 7 days after deletion
+#   - During this time, deleting the parent folder may fail because the project is still technically linked
+resource "stackit_resourcemanager_project" "example_project" {
+  name                = "example-project"
+  owner_email         = "foo.bar@stackit.cloud"
+  parent_container_id = stackit_resourcemanager_folder.example.container_id
 }
