935679cb8d1247b60ddefa6dc54fe0c188ff9e57
Matheus Politano <matheus.correia_politano@mail.schwarz>
2025-09-12 11:06:01 +0200

    remove error

--- stackit/internal/services/cdn/customdomain/resource_test.go
+++ stackit/internal/services/cdn/customdomain/resource_test.go
@@ -272,30 +272,10 @@ func TestBuildCertificatePayload(t *testing.T) {
 			expectErr:      true,
 			expectedErrMsg: "invalid certificate or private key. Please check if the string of the public certificate and private key in PEM format",
 		},
-
-		"success_managed_when_certificate_attributes_are_nil": {
-			model: &CustomDomainModel{
-				Certificate: basetypes.NewObjectValueMust(
-					certificateTypes,
-					map[string]attr.Value{
-						"version":     types.Int64Null(),
-						"certificate": types.StringNull(),
-						"private_key": types.StringNull(),
-					},
-				),
-			},
-			expectedPayload: &cdn.PutCustomDomainPayloadCertificate{
-				PutCustomDomainManagedCertificate: cdn.NewPutCustomDomainManagedCertificate("managed"),
-			},
-			expectErr: false,
-		},
 	}
 
 	for name, tt := range tests {
 		t.Run(name, func(t *testing.T) {
-			if name == "fail_custom_missing_cert_value" {
-				fmt.Println("as")
-			}
 			payload, err := buildCertificatePayload(context.Background(), tt.model)
 			if tt.expectErr {
 				if err == nil {
