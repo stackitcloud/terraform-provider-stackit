a7e376af38307353fe4e88791daa4a10243f4787
Mauritz Uphoff <mauritz.uphoff@stackit.cloud>
2025-09-22 14:02:43 +0200

    feat: add cidr_list attribute to stackit_public_ip_ranges datasource
    Signed-off-by: Mauritz Uphoff <mauritz.uphoff@stackit.cloud>

--- examples/data-sources/stackit_public_ip_ranges/data-source.tf
+++ examples/data-sources/stackit_public_ip_ranges/data-source.tf
@@ -1,1 +1,17 @@
 data "stackit_public_ip_ranges" "example" {}
+
+locals {
+  vpn_cidrs = ["X.X.X.X/32", "X.X.X.X/24"]
+}
+
+# example usage: allow stackit services and customer vpn cidr to access observability apis
+resource "stackit_observability_instance" "example" {
+  project_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  name       = "example-instance"
+  plan_name  = "Observability-Monitoring-Medium-EU01"
+  # Allow all stackit services and customer vpn cidr to access observability apis
+  acl                                    = concat(data.stackit_public_ip_ranges.example.cidr_list, local.vpn_cidrs)
+  metrics_retention_days                 = 90
+  metrics_retention_days_5m_downsampling = 90
+  metrics_retention_days_1h_downsampling = 90
+}
