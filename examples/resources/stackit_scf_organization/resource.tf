eac4ab315b7857131173e5d82e5fe5eaf736ef94
Fabian Spottog <Fabian.Spottog@stackit.cloud>
2025-10-02 13:20:31 +0200

    improve documentation

--- examples/resources/stackit_scf_organization/resource.tf
+++ examples/resources/stackit_scf_organization/resource.tf
@@ -11,8 +11,8 @@ resource "stackit_scf_organization" "example" {
   suspended   = false
 }
 
-# Only use the import statement, if you want to import an existing ske cluster
+# Only use the import statement, if you want to import an existing scf organization
 import {
   to = stackit_scf_organization.import-example
-  id = "${var.project_id},${var.org_id}"
+  id = "${var.project_id},${var.region},${var.org_id}"
 }
