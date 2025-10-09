d25b221c6627697e5f5b5548c14b0312468a73e5
Ruben Hoenle <Ruben.Hoenle@stackit.cloud>
2025-10-09 17:34:41 +0200

    fix(dns): store IDs immediately after provisioning
    relates to STACKITTPR-373

--- stackit/internal/services/dns/recordset/resource.go
+++ stackit/internal/services/dns/recordset/resource.go
@@ -10,7 +10,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework-validators/int64validator"
 	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
-	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
@@ -219,7 +218,10 @@ func (r *recordSetResource) Create(ctx context.Context, req resource.CreateReque
 		core.LogAndAddError(ctx, &resp.Diagnostics, "Error creating record set", fmt.Sprintf("Calling API: %v", err))
 		return
 	}
-	ctx = tflog.SetField(ctx, "record_set_id", *recordSetResp.Rrset.Id)
+
+	utils.SetAndLogStateFields(ctx, &resp.Diagnostics, &resp.State, map[string]interface{}{
+		"record_set_id": *recordSetResp.Rrset.Id,
+	})
 
 	waitResp, err := wait.CreateRecordSetWaitHandler(ctx, r.client, projectId, zoneId, *recordSetResp.Rrset.Id).WaitWithContext(ctx)
 	if err != nil {
@@ -373,9 +375,11 @@ func (r *recordSetResource) ImportState(ctx context.Context, req resource.Import
 		return
 	}
 
-	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("project_id"), idParts[0])...)
-	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("zone_id"), idParts[1])...)
-	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("record_set_id"), idParts[2])...)
+	utils.SetAndLogStateFields(ctx, &resp.Diagnostics, &resp.State, map[string]interface{}{
+		"project_id":    idParts[0],
+		"zone_id":       idParts[1],
+		"record_set_id": idParts[2],
+	})
 	tflog.Info(ctx, "DNS record set state imported")
 }
 
