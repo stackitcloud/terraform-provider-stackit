720a183e07f24f9bae3b5b6ecb2da1670ab15f72
Ruben Hoenle <Ruben.Hoenle@stackit.cloud>
2025-11-04 12:49:27 +0100

    feat(kms): add key resource and datasource
    relates to STACKITTPR-411

---
+++ examples/data-sources/stackit_kms_key/data-source.tf
@@ -0,0 +1,5 @@
+data "stackit_kms_key" "key" {
+  project_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  keyring_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  key_id     = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+}
