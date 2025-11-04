89c440b849f102e57e28f74b319c9856ffec06c3
Ruben Hoenle <Ruben.Hoenle@stackit.cloud>
2025-11-04 12:49:27 +0100

    feat(kms): add wrapping key resource and datasource
    relates to STACKITTPR-416

--- examples/resources/stackit_kms_keyring/resource.tf
+++ examples/resources/stackit_kms_keyring/resource.tf
@@ -1,5 +1,5 @@
 resource "stackit_kms_keyring" "example" {
   project_id   = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-  display_name = "example name"
+  display_name = "example-name"
   description  = "example description"
 }
