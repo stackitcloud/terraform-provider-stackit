1ffc682e0c839813aaf0d07e4e63987a94c92b5f
Devansh Thakur <devansh.thakur@mail.schwarz>
2025-12-08 09:07:58 +0100

    added example of intake runner

---
+++ examples/resources/stackit_intake_runner/resource.tf
@@ -0,0 +1,17 @@
+resource "stackit_intake_runner" "example" {
+  project_id              = var.project_id
+  name                    = "example-runner-full"
+  description             = "An example runner for STACKIT Intake"
+  max_message_size_kib    = 2048
+  max_messages_per_hour   = 1500
+  labels = {
+    "created_by" = "terraform-example"
+    "env"        = "production"
+  }
+  region                  = var.region
+}
+
+import {
+  to = stackit_intake_runner.example
+  id = "${var.project_id},${var.region},${var.runner_id}"
+}
