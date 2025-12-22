f0414ab3b925d37a7c3835a77376109e05304883
Marcel S. Henselin <marcel@henselin.net>
2025-12-22 09:41:03 +0100

    Feat/alpa sqlserver user (#13)
    * fix: update API client references to use sqlserverflexalpha package
    
    * chore: add stackit_sqlserverflexalpha_user resource and instance_id variable
    
    * fix: refactor sqlserverflexalpha user resource and enhance schema with status and default_database
    
    ---------
    
    Co-authored-by: Andre Harms <andre.harms@stackit.cloud>

--- sample/user.tf
+++ sample/user.tf
@@ -1,8 +1,6 @@
-# see other files
-
 resource "stackit_sqlserverflexalpha_user" "ptlsdbuser" {
   project_id  = stackitalpha_postgresflexalpha_instance.ptlsdbsrv.project_id
   instance_id = stackitalpha_postgresflexalpha_instance.ptlsdbsrv.id
   username    = var.db_username
-  roles       = ["login"]
+  roles       = ["createdb", "login", "createrole"]
 }
