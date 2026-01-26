70d08904f3b941fe83bddeac2df35915e6213111
Yago Carlos Fernandez Gou <yago.fernandez_gou@mail.schwarz>
2026-01-26 14:45:43 +0100

    Reshape resource_acc_test

---
+++ stackit/internal/services/intake/testdata/resource-min.tf
@@ -0,0 +1,10 @@
+
+variable "project_id" {}
+variable "name" {}
+
+resource "stackit_intake_runner" "example" {
+    project_id = var.project_id
+    name       = var.name
+    max_message_size_kib    = 1024
+    max_messages_per_hour   = 1000
+}
