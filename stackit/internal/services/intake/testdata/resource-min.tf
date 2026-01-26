002ee74d93603b150cc50ad1137e35e77102597d
Yago Carlos Fernandez Gou <yago.fernandez_gou@mail.schwarz>
2026-01-26 17:11:34 +0100

    lint

--- stackit/internal/services/intake/testdata/resource-min.tf
+++ stackit/internal/services/intake/testdata/resource-min.tf
@@ -3,8 +3,8 @@ variable "project_id" {}
 variable "name" {}
 
 resource "stackit_intake_runner" "example" {
-    project_id = var.project_id
-    name       = var.name
-    max_message_size_kib    = 1024
-    max_messages_per_hour   = 1000
+  project_id = var.project_id
+  name       = var.name
+  max_message_size_kib    = 1024
+  max_messages_per_hour   = 1000
 }
