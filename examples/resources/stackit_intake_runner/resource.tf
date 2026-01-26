f524893916367d0142c82c274fe8b25f07a0cb7e
Yago Carlos Fernandez Gou <yago.fernandez_gou@mail.schwarz>
2026-01-26 17:25:13 +0100

    lint

--- examples/resources/stackit_intake_runner/resource.tf
+++ examples/resources/stackit_intake_runner/resource.tf
@@ -1,14 +1,14 @@
 resource "stackit_intake_runner" "example" {
-  project_id              = var.project_id
-  name                    = "example-runner-full"
-  description             = "An example runner for STACKIT Intake"
-  max_message_size_kib    = 2048
-  max_messages_per_hour   = 1500
+  project_id            = var.project_id
+  name                  = "example-runner-full"
+  description           = "An example runner for STACKIT Intake"
+  max_message_size_kib  = 2048
+  max_messages_per_hour = 1500
   labels = {
     "created_by" = "terraform-example"
     "env"        = "production"
   }
-  region                  = var.region
+  region = var.region
 }
 
 import {
