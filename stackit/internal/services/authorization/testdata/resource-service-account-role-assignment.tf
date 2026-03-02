29ad7a4d04fc18a50fa3a975c10b5a9d178ea67a
Mauritz Uphoff <mauritz.uphoff@stackit.cloud>
2026-03-02 12:42:24 +0100

    feat(iam): resource for service_account role assignment
    Signed-off-by: Mauritz Uphoff <mauritz.uphoff@stackit.cloud>

---
+++ stackit/internal/services/authorization/testdata/resource-service-account-role-assignment.tf
@@ -0,0 +1,26 @@
+variable "name" {}
+variable "act_as_name" {}
+variable "project_id" {}
+variable "role" {}
+
+resource "stackit_service_account" "iam" {
+  project_id = var.project_id
+  name       = var.name
+}
+
+resource "stackit_authorization_project_role_assignment" "pr_sa" {
+  resource_id = var.project_id
+  role        = "editor"
+  subject     = stackit_service_account.iam.email
+}
+
+resource "stackit_service_account" "act_as" {
+  project_id = var.project_id
+  name       = var.act_as_name
+}
+
+resource "stackit_authorization_service_account_role_assignment" "sa" {
+  resource_id = stackit_service_account.iam.service_account_id
+  role        = var.role
+  subject     = stackit_service_account.act_as.email
+}
