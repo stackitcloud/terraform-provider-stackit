4882f5030afa51aed89dd3a7b6ad3460a8e4bf99
Manuel Vaas <manuel.vaas@stackit.cloud>
2026-03-06 18:54:00 +0100

    fixed or flagged linter findings

--- stackit/internal/services/iaas/image/resource.go
+++ stackit/internal/services/iaas/image/resource.go
@@ -865,7 +865,7 @@ func uploadImage(ctx context.Context, diags *diag.Diagnostics, filePath, uploadU
 		return fmt.Errorf("stat file: %w", err)
 	}
 
-	req, err := http.NewRequest(http.MethodPut, uploadURL, bufio.NewReader(file))
+	req, err := http.NewRequest(http.MethodPut, uploadURL, bufio.NewReader(file)) //nolint:gosec // file upload
 	if err != nil {
 		return fmt.Errorf("create upload request: %w", err)
 	}
@@ -873,7 +873,7 @@ func uploadImage(ctx context.Context, diags *diag.Diagnostics, filePath, uploadU
 	req.ContentLength = stat.Size()
 
 	client := &http.Client{}
-	resp, err := client.Do(req)
+	resp, err := client.Do(req) //nolint:gosec // file upload
 	if err != nil {
 		return fmt.Errorf("upload image: %w", err)
 	}
