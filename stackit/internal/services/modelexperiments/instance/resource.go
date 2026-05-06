44465001c25fe9d2d48538d0ff6ebe24fd2dde5b
Peter Paul Schaffrath <peter_paul.schaffrath@digits.schwarz>
2026-05-06 11:34:30 +0200

    Add Read update delete func for mlflow instance

--- stackit/internal/services/modelexperiments/instance/resource.go
+++ stackit/internal/services/modelexperiments/instance/resource.go
@@ -84,7 +84,7 @@ func (i *instanceResource) Configure(ctx context.Context, req resource.Configure
 	}
 	i.client = apiClient
 	i.serviceEnablementClient = serviceEnablementClient
-	tflog.Info(ctx, "Model-Serving auth token client configured")
+	tflog.Info(ctx, "Model experiments client configured")
 }
 
 func (i *instanceResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
@@ -174,7 +174,7 @@ func (i *instanceResource) Schema(_ context.Context, _ resource.SchemaRequest, r
 }
 
 // Create creates the resource and sets the initial Terraform state.
-func (i *instanceResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+func (i *instanceResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) { // nolint:gocritic // function signature required by Terraform
 	// Retrieve values from plan
 	var model Model
 	diags := req.Plan.Get(ctx, &model)
@@ -226,7 +226,7 @@ func (i *instanceResource) Create(ctx context.Context, req resource.CreateReques
 
 	payload, err := toCreatePayload(&model)
 	if err != nil {
-		core.LogAndAddError(ctx, &resp.Diagnostics, "Error creating AI model serving auth token", fmt.Sprintf("Creating API payload: %v", err))
+		core.LogAndAddError(ctx, &resp.Diagnostics, "Error creating AI model experiments instance", fmt.Sprintf("Creating API payload: %v", err))
 		return
 	}
 
@@ -257,13 +257,16 @@ func (i *instanceResource) Create(ctx context.Context, req resource.CreateReques
 		return
 	}
 
-	mapValue, diags := types.MapValueFrom(ctx, types.StringType, createInstanceResp.Instance.Labels)
-	if diags.HasError() {
-		return
+	var mapValue basetypes.MapValue
+	if createInstanceResp.Instance.Labels != nil {
+		mapValue, diags = types.MapValueFrom(ctx, types.StringType, createInstanceResp.Instance.Labels)
+		if diags.HasError() {
+			return
+		}
 	}
 
 	//If model experiments instance is impaired, write state avoid dangling resources and return
-	waitResp, err := CreateModelExperimentsWaitHandler(ctx, i.client, region, projectId, instanceId).WaitWithContext(ctx)
+	waitResp, err := CreateMExpInstanceWaitHandler(ctx, i.client, region, projectId, instanceId).WaitWithContext(ctx)
 	if err != nil {
 		mapCreateResponse(createInstanceResp, waitResp, &model, region, mapValue)
 		diags = resp.State.Set(ctx, model)
@@ -287,19 +290,188 @@ func (i *instanceResource) Create(ctx context.Context, req resource.CreateReques
 		return
 	}
 
-	tflog.Info(ctx, "Model Experiments instance created")
+	tflog.Info(ctx, "Model experiments instance created")
 }
 
 // Read refreshes the Terraform state with the latest data.
-func (i *instanceResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+func (i *instanceResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) { // nolint:gocritic // function signature required by Terraform
+	var model Model
+	diags := req.State.Get(ctx, &model)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	ctx = core.InitProviderContext(ctx)
+
+	projectId := model.ProjectId.ValueString()
+	instanceId := model.InstanceId.ValueString()
+	if instanceId == "" {
+		// Resource not yet created; ID is unknown.
+		resp.State.RemoveResource(ctx)
+		return
+	}
+	region := i.providerData.GetRegionWithOverride(model.Region)
+
+	ctx = tflog.SetField(ctx, "project_id", projectId)
+	ctx = tflog.SetField(ctx, "instance_id", instanceId)
+	ctx = tflog.SetField(ctx, "region", region)
+
+	getInstanceResp, err := i.client.DefaultAPI.GetInstance(ctx, projectId, region, instanceId).Execute()
+	if err != nil {
+		var oapiErr *oapierror.GenericOpenAPIError
+		if errors.As(err, &oapiErr) {
+			if oapiErr.StatusCode == http.StatusNotFound {
+				// Remove the resource from the state so Terraform will recreate it
+				resp.State.RemoveResource(ctx)
+				return
+			}
+		}
+
+		core.LogAndAddError(ctx, &resp.Diagnostics, "Error reading AI model experiments instance", fmt.Sprintf("Calling API: %v", err))
+		return
+	}
+	ctx = core.LogResponse(ctx)
+
+	err = mapInstance(ctx, getInstanceResp.Instance, &model)
+	if err != nil {
+		core.LogAndAddError(ctx, &resp.Diagnostics, "Error updating AI model experiments instance", fmt.Sprintf("Processing API payload: %v", err))
+		return
+	}
+
+	diags = resp.State.Set(ctx, model)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	tflog.Info(ctx, "Model experiments instance read")
+
 }
 
 // Update updates the resource and sets the updated Terraform state on success.
-func (i *instanceResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+func (i *instanceResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) { // nolint:gocritic // function signature required by Terraform
+	// Retrieve values from plan
+	var model Model
+	diags := req.Plan.Get(ctx, &model)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Get current state
+	var state Model
+	diags = req.State.Get(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	ctx = core.InitProviderContext(ctx)
+
+	projectId := state.ProjectId.ValueString()
+	instanceId := state.InstanceId.ValueString()
+
+	region := i.providerData.GetRegionWithOverride(model.Region)
+
+	ctx = tflog.SetField(ctx, "project_id", projectId)
+	ctx = tflog.SetField(ctx, "instance_id", instanceId)
+	ctx = tflog.SetField(ctx, "region", region)
+
+	payload, err := toUpdatePayload(&model)
+	if err != nil {
+		core.LogAndAddError(ctx, &resp.Diagnostics, "Error updating AI model experiments instance", fmt.Sprintf("Creating API payload: %v", err))
+		return
+	}
+
+	updateInstanceResp, err := i.client.DefaultAPI.PartialUpdateInstance(ctx, projectId, region, instanceId).PartialUpdateInstancePayload(*payload).Execute()
+	if err != nil {
+		var oapiErr *oapierror.GenericOpenAPIError
+		if errors.As(err, &oapiErr) {
+			if oapiErr.StatusCode == http.StatusNotFound {
+				// Remove the resource from the state so Terraform will recreate it
+				resp.State.RemoveResource(ctx)
+				return
+			}
+		}
+
+		core.LogAndAddError(
+			ctx,
+			&resp.Diagnostics,
+			"Error updating AI model experiments instance",
+			fmt.Sprintf(
+				"Calling API: %v, instanceId: %s, region: %s, projectId: %s",
+				err,
+				instanceId,
+				region,
+				projectId,
+			),
+		)
+		return
+	}
+
+	ctx = core.LogResponse(ctx)
+
+	err = mapInstance(ctx, updateInstanceResp.Instance, &model)
+	if err != nil {
+		core.LogAndAddError(ctx, &resp.Diagnostics, "Error updating AI model experiments instance", fmt.Sprintf("Processing API payload: %v", err))
+		return
+	}
+
+	diags = resp.State.Set(ctx, model)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	tflog.Info(ctx, "Model experiments instance updated")
 }
 
 // Delete deletes the resource and removes the Terraform state on success.
-func (i *instanceResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+func (i *instanceResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) { // nolint:gocritic // function signature required by Terraform
+	// Retrieve values from plan
+	var model Model
+	diags := req.State.Get(ctx, &model)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	ctx = core.InitProviderContext(ctx)
+
+	projectId := model.ProjectId.ValueString()
+	instanceId := model.InstanceId.ValueString()
+
+	region := i.providerData.GetRegionWithOverride(model.Region)
+
+	ctx = tflog.SetField(ctx, "project_id", projectId)
+	ctx = tflog.SetField(ctx, "instance_id", instanceId)
+	ctx = tflog.SetField(ctx, "region", region)
+
+	_, err := i.client.DefaultAPI.DeleteInstance(ctx, projectId, region, instanceId).Execute()
+	if err != nil {
+		var oapiErr *oapierror.GenericOpenAPIError
+		if errors.As(err, &oapiErr) {
+			if oapiErr.StatusCode == http.StatusNotFound {
+				resp.State.RemoveResource(ctx)
+				return
+			}
+		}
+
+		core.LogAndAddError(ctx, &resp.Diagnostics, "Error deleting AI model experiments instance", fmt.Sprintf("Calling API: %v", err))
+		return
+	}
+
+	ctx = core.LogResponse(ctx)
+
+	_, err = DeleteMExpInstanceWaitHandler(ctx, i.client, region, projectId, instanceId).
+		WaitWithContext(ctx)
+	if err != nil {
+		core.LogAndAddError(ctx, &resp.Diagnostics, "Error deleting AI model experiments instance", fmt.Sprintf("Waiting for instance to be deleted: %v", err))
+		return
+	}
+
+	tflog.Info(ctx, "Model experiments instance deleted")
 }
 
 func mapCreateResponse(instanceCreateResp *modelexperiments.CreateInstanceResponse, waitResp *modelexperiments.GetInstanceResponse, model *Model, region string, labels basetypes.MapValue) error {
@@ -333,6 +505,29 @@ func mapCreateResponse(instanceCreateResp *modelexperiments.CreateInstanceRespon
 	return nil
 }
 
+func mapInstance(ctx context.Context, instance modelexperiments.Instance, model *Model) error {
+	if model == nil {
+		return fmt.Errorf("model input is nil")
+	}
+
+	mapValue, diags := types.MapValueFrom(ctx, types.StringType, instance.Labels)
+	if diags.HasError() {
+		return fmt.Errorf("failure in mapping labels")
+	}
+
+	model.Id = utils.BuildInternalTerraformId(model.ProjectId.ValueString(), model.Region.ValueString(), model.InstanceId.ValueString())
+	model.InstanceId = types.StringValue(instance.Id)
+	model.Name = types.StringValue(instance.Name)
+	model.State = types.StringValue(instance.State)
+	model.Description = types.StringPointerValue(instance.Description)
+	model.DeletedExperimentRetention = types.StringPointerValue(instance.DeletedExperimentRetention)
+	model.BucketName = types.StringPointerValue(instance.BucketName)
+	model.ErrorMessage = types.StringPointerValue(instance.ErrorMessage)
+	model.Labels = mapValue
+
+	return nil
+}
+
 func toCreatePayload(model *Model) (*modelexperiments.CreateInstancePayload, error) {
 	if model == nil {
 		return nil, fmt.Errorf("nil model")
@@ -352,7 +547,24 @@ func toCreatePayload(model *Model) (*modelexperiments.CreateInstancePayload, err
 	}, nil
 }
 
-func CreateModelExperimentsWaitHandler(ctx context.Context, a *modelexperiments.APIClient, region, projectId, instanceId string) *wait.AsyncActionHandler[modelexperiments.GetInstanceResponse] {
+func toUpdatePayload(model *Model) (*modelexperiments.PartialUpdateInstancePayload, error) {
+	if model == nil {
+		return nil, fmt.Errorf("nil model")
+	}
+	modelLabels := model.Labels.Elements()
+	labels, err := conversion.ToOptStringMap(modelLabels)
+	if err != nil {
+		return nil, fmt.Errorf("converting to Go map: %w", err)
+	}
+	return &modelexperiments.PartialUpdateInstancePayload{
+		Name:                       model.Name.ValueStringPointer(),
+		Description:                model.Description.ValueStringPointer(),
+		DeletedExperimentRetention: model.DeletedExperimentRetention.ValueStringPointer(),
+		Labels:                     labels,
+	}, nil
+}
+
+func CreateMExpInstanceWaitHandler(ctx context.Context, a *modelexperiments.APIClient, region, projectId, instanceId string) *wait.AsyncActionHandler[modelexperiments.GetInstanceResponse] {
 	handler := wait.New(func() (waitFinished bool, response *modelexperiments.GetInstanceResponse, err error) {
 		getInstanceResp, err := a.DefaultAPI.GetInstance(ctx, region, projectId, instanceId).Execute()
 		if err != nil {
@@ -367,6 +579,30 @@ func CreateModelExperimentsWaitHandler(ctx context.Context, a *modelexperiments.
 
 		return false, nil, nil
 	})
+
+	handler.SetTimeout(10 * time.Minute)
+
+	return handler
+}
+
+func DeleteMExpInstanceWaitHandler(ctx context.Context, a *modelexperiments.APIClient, region, projectId, instanceId string) *wait.AsyncActionHandler[modelexperiments.GetInstanceResponse] {
+	handler := wait.New(
+		func() (waitFinished bool, response *modelexperiments.GetInstanceResponse, err error) {
+			_, err = a.DefaultAPI.GetInstance(ctx, region, projectId, instanceId).Execute()
+			if err != nil {
+				var oapiErr *oapierror.GenericOpenAPIError
+				if errors.As(err, &oapiErr) {
+					if oapiErr.StatusCode == http.StatusNotFound {
+						return true, nil, nil
+					}
+				}
+
+				return false, nil, err
+			}
+
+			return false, nil, nil
+		},
+	)
 
 	handler.SetTimeout(10 * time.Minute)
 
