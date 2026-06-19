e22fbd0c0ee21b12d280df3e0922f2cc88614c12
Alexander Dahmen <alexander.dahmen@inovex.de>
2026-06-19 13:32:18 +0200

    fix(iaas): IaaS acceptance tests fixes
    Create a project to network interface and server acceptance tests.
    Adjust acceptance tests to manually check ipv4_nameservers.
    Update image_v2 acceptance test.
    
    STACKITTPR-673
    
    Signed-off-by: Alexander Dahmen <alexander.dahmen@inovex.de>

--- stackit/internal/services/iaas/testdata/resource-network-min.tf
+++ stackit/internal/services/iaas/testdata/resource-network-min.tf
@@ -1,7 +1,17 @@
 variable "project_id" {}
 variable "name" {}
 
+variable "parent_container_id" {}
+variable "project_name" {}
+variable "owner_email" {}
+
+resource "stackit_resourcemanager_project" "example" {
+  parent_container_id = var.parent_container_id
+  name                = var.project_name
+  owner_email         = var.owner_email
+}
+
 resource "stackit_network" "network" {
-  project_id = var.project_id
+  project_id = stackit_resourcemanager_project.example.project_id
   name       = var.name
 }
