659b15d8f73849f5934d65375589a4d1eeb1ce54
Gökce Gök Klingel <161626272+GokceGK@users.noreply.github.com>
2026-07-14 11:44:59 +0200

    fix(authorization): fix custom role examples (#1581)
    relates to #1579

--- examples/data-sources/stackit_authorization_project_custom_role/data-source.tf
+++ examples/data-sources/stackit_authorization_project_custom_role/data-source.tf
@@ -1,5 +1,4 @@
 data "stackit_authorization_project_custom_role" "example" {
   resource_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-  name        = "my.custom.role"
+  role_id     = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
 }
-
