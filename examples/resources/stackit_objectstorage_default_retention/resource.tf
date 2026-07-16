5f1f2b71c755e5ce31693a62e2d130e7db4b49d4
Jonas Schlecht <jonas.schlecht_ext@external.digits.schwarz>
2026-07-16 10:48:48 +0200

    fix(objectstorage): fix default retentions example
    Relates to STACKITTPR-757

--- examples/resources/stackit_objectstorage_default_retention/resource.tf
+++ examples/resources/stackit_objectstorage_default_retention/resource.tf
@@ -1,10 +1,17 @@
-resource "stackit_objectstorage_compliance_lock" "example_bucket" {
+resource "stackit_objectstorage_compliance_lock" "example_lock" {
   project_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+}
+
+resource "stackit_objectstorage_bucket" "bucket_object_lock" {
+  depends_on  = [stackit_objectstorage_compliance_lock.example_lock]
+  project_id  = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  name        = "example-bucket-with-lock"
+  object_lock = true
 }
 
 resource "stackit_objectstorage_default_retention" "example" {
   project_id  = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-  bucket_name = stackit_objectstorage_compliance_lock.example_bucket.name
-  days        = var.retention_days
-  mode        = var.retention_mode
+  bucket_name = stackit_objectstorage_bucket.bucket_object_lock.name
+  days        = 2
+  mode        = "GOVERNANCE"
 }
