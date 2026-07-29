2ae9cd6af1a9b048d048c4197cc6256aa106904e
Carlo Goetz <carlo.goetz@inovex.de>
2026-07-29 15:58:04 +0200

    fix(validate): delete unused full version regex

--- stackit/internal/validate/validate.go
+++ stackit/internal/validate/validate.go
@@ -22,13 +22,6 @@ import (
 	"github.com/stackitcloud/terraform-provider-stackit/stackit/internal/utils"
 )
 
-const (
-	FullVersionRegex = `^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)` +
-		`(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)` +
-		`(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?` +
-		`(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$`
-)
-
 type Validator struct {
 	description         string
 	markdownDescription string
