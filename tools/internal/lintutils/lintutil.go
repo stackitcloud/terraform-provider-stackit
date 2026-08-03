6d22f26df0ab81c22b999cd396892a296e436811
Ruben Hoenle <Ruben.Hoenle@digits.schwarz>
2026-08-03 12:09:53 +0200

    resolve open todo, add unit tests

--- tools/internal/lintutils/lintutil.go
+++ tools/internal/lintutils/lintutil.go
@@ -3,6 +3,7 @@ package lintutils
 import (
 	"go/ast"
 	"go/types"
+	"slices"
 	"strings"
 )
 
@@ -40,44 +41,20 @@ func GetCallInfo(call *ast.CallExpr, info *types.Info) (string, string) {
 }
 
 // IsTerraformLifecycleMethod is used to check whether an AST function declaration is a Terraform resource
-// lifecycle implementation (CRUD)
-func IsTerraformLifecycleMethod(funcDecl *ast.FuncDecl) bool {
+// lifecycle implementation. If no methodNamesFilter values are given it checks for all methods (CRUD).
+func IsTerraformLifecycleMethod(funcDecl *ast.FuncDecl, methodNamesFilter ...string) bool {
 	// 1. Must be a method (have a receiver)
 	if funcDecl.Recv == nil {
 		return false
 	}
 
 	// 2. Check if it is a Terraform Provider Framework CRUD method
-	name := funcDecl.Name.Name
-	if name != "Create" && name != "Read" && name != "Update" && name != "Delete" {
-		return false
+	if len(methodNamesFilter) == 0 {
+		// use the default fallback values if no values were given
+		methodNamesFilter = []string{"Create", "Read", "Update", "Delete"}
 	}
 
-	// 3. Ensure the method has exactly 3 parameters (e.g., ctx, req, resp)
-	if funcDecl.Type.Params == nil || len(funcDecl.Type.Params.List) != 3 {
-		return false
-	}
-
-	// 4. Verify it's actually a Terraform framework method by checking if
-	// param 2 ends with "Request" and param 3 ends with "Response"
-	if !hasSuffixType(funcDecl.Type.Params.List[1], "Request") || !hasSuffixType(funcDecl.Type.Params.List[2], "Response") {
-		return false
-	}
-
-	return true
-}
-
-// IsTerraformLifecycleMethod is used to check whether an AST function declaration is a Terraform resource
-// lifecycle implementation (CRUD)
-func IsTerraformLifecycleMethodCreateOnlyTODO(funcDecl *ast.FuncDecl) bool {
-	// 1. Must be a method (have a receiver)
-	if funcDecl.Recv == nil {
-		return false
-	}
-
-	// 2. Check if it is a Terraform Provider Framework CRUD method
-	name := funcDecl.Name.Name
-	if name != "Create" {
+	if !slices.Contains(methodNamesFilter, funcDecl.Name.Name) {
 		return false
 	}
 
