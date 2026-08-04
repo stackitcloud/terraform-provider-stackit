86d564b274da58de42fd936a8dff3125f9305ee4
Alexander Dahmen <alexander.dahmen@inovex.de>
2026-08-04 08:28:38 +0200

    feat(telemetrylink): Add write only attributes for accessToken
    STACKITTPR-751
    
    Signed-off-by: Alexander Dahmen <alexander.dahmen@inovex.de>

--- stackit/internal/services/telemetrylink/testdata/resource-max.tf
+++ stackit/internal/services/telemetrylink/testdata/resource-max.tf
@@ -5,6 +5,7 @@ variable "resource_id" {}
 variable "region" {}
 variable "display_name" {}
 variable "description" {}
+variable "access_token_wo_version" {}
 
 resource "stackit_telemetryrouter_instance" "router" {
   project_id   = var.project_id
@@ -20,11 +21,13 @@ resource "stackit_telemetryrouter_access_token" "accessToken" {
 }
 
 resource "stackit_telemetrylink" "link" {
-  resource_type       = var.resource_type
-  resource_id         = var.resource_id
-  region              = var.region
-  display_name        = var.display_name
-  description         = var.description
-  access_token        = stackit_telemetryrouter_access_token.accessToken.access_token
-  telemetry_router_id = stackit_telemetryrouter_instance.router.instance_id
+  resource_type = var.resource_type
+  resource_id   = var.resource_id
+  region        = var.region
+  display_name  = var.display_name
+  description   = var.description
+  # in the MIN test we use the legacy field, in the MAX test the write-only field
+  access_token_wo         = stackit_telemetryrouter_access_token.accessToken.access_token
+  access_token_wo_version = var.access_token_wo_version
+  telemetry_router_id     = stackit_telemetryrouter_instance.router.instance_id
 }
