de8f7dc42856afbbe4785b9013259240658202da
Jonas Schlecht <jonas.schlecht_ext@external.digits.schwarz>
2026-08-18 11:23:06 +0200

    refactor: refactor onlyIfBoolValidator to be in the same style as other validators
    Relates to STACKITTPR-786

--- stackit/internal/validate/validate.go
+++ stackit/internal/validate/validate.go
@@ -400,53 +400,39 @@ func IsLowercased() *Validator {
 	}
 }
 
-// OnlyAllowedIfBoolEqualsValidator prevents that this string attribute is set if a target bool does not equal the specified value.
-type OnlyAllowedIfBoolEqualsValidator struct {
-	Target path.Expression
-	Value  *bool
-}
+// OnlyAllowedIfBoolEquals returns a Validator that prevents this string attribute
+// from being set if the target bool attribute does not equal the specified value.
+// If value is nil, no validation is performed.
+func OnlyAllowedIfBoolEquals(target path.Expression, value *bool) *Validator {
+	description := "the attribute can only be set if the boolean is set to the provided value"
 
-// Ensure the validator implements the String validator interface
-var _ validator.String = OnlyAllowedIfBoolEqualsValidator{}
+	return &Validator{
+		description: description,
+		validate: func(ctx context.Context, req validator.StringRequest, resp *validator.StringResponse) {
+			expression := req.PathExpression.Merge(target)
 
-func (v OnlyAllowedIfBoolEqualsValidator) Description(_ context.Context) string {
-	return "The attribute can only be set if the boolean is set to the provided value."
-}
+			matchedPaths, diags := req.Config.PathMatches(ctx, expression)
+			resp.Diagnostics.Append(diags...)
 
-func (v OnlyAllowedIfBoolEqualsValidator) MarkdownDescription(ctx context.Context) string {
-	return v.Description(ctx)
-}
+			for _, targetPath := range matchedPaths {
+				var targetBool types.Bool
+				diags := req.Config.GetAttribute(ctx, targetPath, &targetBool)
+				resp.Diagnostics.Append(diags...)
 
-func (v OnlyAllowedIfBoolEqualsValidator) ValidateString(ctx context.Context, req validator.StringRequest, resp *validator.StringResponse) { // nolint:gocritic // function signature required by Terraform
-	expression := req.PathExpression.Merge(v.Target)
+				// nothing to validate against: no expected value given or target not set in the config
+				if resp.Diagnostics.HasError() || value == nil || targetBool.IsNull() || targetBool.IsUnknown() {
+					return
+				}
 
-	matchedPaths, diags := req.Config.PathMatches(ctx, expression)
-	resp.Diagnostics.Append(diags...)
-
-	for _, target := range matchedPaths {
-		var targetBool types.Bool
-		diags := req.Config.GetAttribute(ctx, target, &targetBool)
-		resp.Diagnostics.Append(diags...)
-
-		// nothing to validate against: no expected value given or target not set in the config
-		if resp.Diagnostics.HasError() || v.Value == nil || targetBool.IsNull() || targetBool.IsUnknown() {
-			return
-		}
-
-		if targetBool.ValueBool() != *v.Value && !req.ConfigValue.IsNull() {
-			resp.Diagnostics.AddAttributeError(
-				req.Path,
-				"Attribute can not be set",
-				fmt.Sprintf("This attribute can only be configured when %q is set to %t.", target.String(), *v.Value),
-			)
-		}
-	}
-}
-
-func OnlyAllowedIfBoolEquals(target path.Expression, value *bool) validator.String {
-	return OnlyAllowedIfBoolEqualsValidator{
-		Target: target,
-		Value:  value,
+				if targetBool.ValueBool() != *value {
+					resp.Diagnostics.AddAttributeError(
+						req.Path,
+						"Attribute can not be set",
+						fmt.Sprintf("This attribute can only be configured when %q is set to %t.", targetPath.String(), *value),
+					)
+				}
+			}
+		},
 	}
 }
 
